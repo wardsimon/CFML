cb533a5c5a90baec72a80c47d96e07be1ccf7ea3
Juan Rodriguez-Carvajal <jrc@ill.fr>
2008-10-17 11:30:23 +0000

    Deleted for re-organization
    git-svn-id: https://forge.ill.fr/svn/crysfml@7 06508225-4b59-0410-a2a9-fdab55894d57

--- SpaceGroups/make_space_group_info.bat
+++
@@ -1,4 +0,0 @@
-del *.obj
-del *.mod
-lf95 -c space_group_info.f90    -info  -o1 -chk -mod ".;C:\crysFML\lahey\libC"
-lf95 *.obj -out space_group_info  -o1 -mod ".;C:\crysFML\lahey\LibC" -lib C:\crysFML\lahey\libC\crysFML  -chk
--- SpaceGroups/make_subgroups.bat
+++
@@ -1,4 +0,0 @@
-del *.obj
-del *.mod
-lf95 -c subgroups.f90    -info  -o1 -chk -mod ".;C:\crysFML\lahey\libC"
-lf95 *.obj -out subgroups  -o1 -mod ".;C:\crysFML\lahey\LibC" -lib C:\crysFML\lahey\libC\crysFML  -chk
