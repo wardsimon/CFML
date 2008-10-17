836c18b728cef2f6e0cb084e2b2e0bd9f01f32df
Juan Rodriguez-Carvajal <jrc@ill.fr>
2008-10-17 11:29:45 +0000

    deleted for reorganisation
    git-svn-id: https://forge.ill.fr/svn/crysfml@6 06508225-4b59-0410-a2a9-fdab55894d57

--- hkl_gen/make_hkl_gen.bat
+++
@@ -1,4 +0,0 @@
-del *.obj
-del *.mod
-lf95 -c hkl_gen.f90    -info  -o1 -chk -mod ".;C:\crysFML\lahey\libC"
-lf95 *.obj -out hkl_gen  -o1 -mod ".;C:\crysFML\lahey\LibC" -lib C:\crysFML\lahey\libC\crysFML  -chk
