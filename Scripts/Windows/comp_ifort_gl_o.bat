d1f36fb02721e9016901fb519b2245c5522b4406
Javier Gonzalez-Platas <jplatas@ull.edu.es>
2008-11-13 14:33:40 +0000

    Actualizating Windows Scripts
    git-svn-id: https://forge.ill.fr/svn/crysfml@75 06508225-4b59-0410-a2a9-fdab55894d57

--- Scripts/Windows/comp_ifort_gl_o.bat
+++ Scripts/Windows/comp_ifort_gl_o.bat
@@ -4,23 +4,23 @@ rem
    @echo off
    cd ../../cfgl
 rem
-   ifort /c WCFGL_constant.f90           /Ox /nologo /I. /I..\..\Intel\LibW
-   ifort /c WCFGL_atomic_table.f90       /Ox /nologo /I. /I..\..\Intel\LibW
-   ifort /c WCFGL_objects_definition.f90 /Ox /nologo /I. /Ic:\wint\lib.if8
-   ifort /c WCFGL_geometry.f90           /Ox /nologo /I. /I..\..\Intel\LibW
-   ifort /c WCFGL_Chull3D.f90            /Ox /nologo /I. /Ic:\wint\lib.if8
-   ifort /c WCFGL_quaternion.f90         /Ox /nologo /I. /I..\..\Intel\LibW
-   ifort /c WCFGL_trackball.f90          /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_metrix.f90             /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_glatom.f90             /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_glbond.f90             /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_atom_tree.f90          /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_matom_tree.f90         /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_bond_tree.f90          /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_glpoly.f90             /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_poly_tree.f90          /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_display.f90            /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
-   ifort /c WCFGL_IO.f90                 /Ox /nologo /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_constant.f90           /O2 /nologo /Qvec-report0 /I. /I..\..\Intel\LibW
+   ifort /c WCFGL_atomic_table.f90       /O2 /nologo /Qvec-report0 /I. /I..\..\Intel\LibW
+   ifort /c WCFGL_objects_definition.f90 /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8
+   ifort /c WCFGL_geometry.f90           /O2 /nologo /Qvec-report0 /I. /I..\..\Intel\LibW
+   ifort /c WCFGL_Chull3D.f90            /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8
+   ifort /c WCFGL_quaternion.f90         /O2 /nologo /Qvec-report0 /I. /I..\..\Intel\LibW
+   ifort /c WCFGL_trackball.f90          /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_metrix.f90             /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_glatom.f90             /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_glbond.f90             /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_atom_tree.f90          /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_matom_tree.f90         /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_bond_tree.f90          /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_glpoly.f90             /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_poly_tree.f90          /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_display.f90            /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
+   ifort /c WCFGL_IO.f90                 /O2 /nologo /Qvec-report0 /I. /Ic:\wint\lib.if8 /I..\..\Intel\LibW >> out
 rem
    echo **---- CrysFGL Library: Winteracter version ----**
 rem
---
+++ Scripts/Windows/make_all.bat
@@ -0,0 +1,18 @@
+@echo off
+rem
+rem Crystallographic Fortran Modules Library
+rem
+   if not x%1 == x goto CONT
+rem
+   cls
+   echo MAKE_ALL: Making the CrysFML and CrysFGL Libraries
+   echo Syntax: make_crysfml [f95/lf95/g95/ifort/gfortran] 
+   goto FIN
+rem
+:CONT
+   call make_crysfml %1 %2
+   call make_crysfml %1 win %2
+   call make_crysfgl %1 %2
+   cd ..\..\..
+rem
+:FIN
