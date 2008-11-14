03474f6a33f495b599d1a9de351229c26b24d14e
Javier Gonzalez-Platas <jplatas@ull.edu.es>
2008-11-14 18:20:14 +0000

    Updating StructureFactors example
    git-svn-id: https://forge.ill.fr/svn/crysfml@100 06508225-4b59-0410-a2a9-fdab55894d57

--- Program_Examples/StructureFactors/make_calc_sfac.bat
+++ Program_Examples/StructureFactors/make_calc_sfac.bat
@@ -1,3 +1,48 @@
-del *.obj
-lf95 -c calc_sfac.f90  -tp -nomap -stchk -nchk -o1 -mod ".;c:\CrysFML\lahey\LibC"
-lf95  *.obj -out calc_sfac -tp -nomap -stchk -nchk -o1 -mod ".;c:\CrysFML\lahey\LibC" -lib c:\CrysFML\lahey\LibC\CrysFML
+@echo off
+rem ****
+rem ****---- Compilation for CALC_SFAC Program ----****
+rem ****
+rem **** Author: JRC + JGP
+rem **** Revision: Nov-2008
+rem ****
+rem
+   if not x%1 == x goto CONT
+   cls
+   echo    MAKE_Calc_SFac: Make CALC_SFAC Compilation
+   echo    Syntax: make_calc_sfac [f95/lf95/g95/gfrotran/ifort]
+   goto END
+rem
+:CONT
+   if x%1 == xf95       goto F95
+   if x%1 == xlf95      goto LF95
+   if x%1 == xg95       goto G95
+   if x%1 == xgfortran  goto GFOR
+   if x%1 == xifort     goto IFORT
+   goto END
+rem
+rem ****---- Absoft Compiler ----****
+:F95
+   goto END
+rem
+rem ****---- Lahey Compiler ----****
+:LF95
+   lf95 -c calc_sfac.f90  -tp -nomap -stchk -nchk -o1 -mod ".;c:\CrysFML\lahey\LibC"
+   lf95  *.obj -out calc_sfac -tp -nomap -stchk -nchk -o1 -mod ".;c:\CrysFML\lahey\LibC" -lib c:\CrysFML\lahey\LibC\CrysFML
+   goto END
+rem      
+rem ****---- Intel Compiler ----****
+:IFORT
+   ifort /c calc_sfac.f90 /O2 /nologo /IC:\CrysFML\Intel\LibC
+   ifort /exe:calc_sfac *.obj C:\CrysFML\Intel\LibC\crysfml.lib
+   goto END
+rem
+rem **---- G95 Compiler ----**
+:G95
+   goto END
+rem
+rem **---- GFORTRAN Compiler ----**
+:GFOR
+   goto END
+rem
+:END
+   del *.obj *.mod *.o *.map *.bak > nul
