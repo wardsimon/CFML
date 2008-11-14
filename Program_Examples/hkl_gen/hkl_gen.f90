bfbed1bd14e4feebb263dfd5860e9b8551029569
Javier Gonzalez-Platas <jplatas@ull.edu.es>
2008-11-14 17:00:51 +0000

    git-svn-id: https://forge.ill.fr/svn/crysfml@98 06508225-4b59-0410-a2a9-fdab55894d57

--- Program_Examples/hkl_gen/hkl_gen.f90
+++ Program_Examples/hkl_gen/hkl_gen.f90
@@ -1,115 +1,150 @@
-!---------------------------------------
-!  Example of simple program using CFML
-!---------------------------------------
-  Program Test_HKL_GEN
-   use Math_gen, only : asind
+!!----
+!!---- Program: HKL_GEN
+!!----          Example of simple program using CFML
+!!----
+!!---- Author: Juan Rodriguez-Carvajal
+!!---- Revision: Nov-2008
+!!
+Program Test_HKL_GEN
+   !---- Use Modules ----!
+   use Math_gen,                  only : asind
    use Crystallographic_Symmetry, only: Space_Group_Type, set_SpaceGroup, &
                                         Write_SpaceGroup
-   use Crystal_types, only: Crystal_Cell_Type,set_crystal_cell,Write_Crystal_Cell
-   use Reflections_utilities, only: Reflect_Type, get_maxnumref, HKL_uni !HKL_gen
+   use Crystal_types,             only: Crystal_Cell_Type,set_crystal_cell,Write_Crystal_Cell
+   use Reflections_utilities,     only: Reflect_Type, get_maxnumref, HKL_uni !HKL_gen
 
+   !---- Variables ----!
    implicit none
 
-        character(len=1)           :: car
-        character(len=12)          :: name_file
-        character(len=20)          :: spgr
-        character(len=35)          :: texto
-        type (Space_Group_Type)    :: grp_espacial
-        type (Crystal_Cell_Type)   :: cell
-        integer                    :: i,num , ier, MaxNumRef
-        real,dimension(3)          :: celda, angulo
-        real                       :: val1,val2,lambda,angle_2theta
-        logical                    :: info
+   character(len=1)           :: car
+   character(len=12)          :: name_file
+   character(len=20)          :: spgr
+   character(len=35)          :: texto
+   type (Space_Group_Type)    :: grp_espacial
+   type (Crystal_Cell_Type)   :: cell
+   integer                    :: i,num , ier, MaxNumRef
+   real,dimension(3)          :: celda, angulo
+   real                       :: val1,val2,lambda,angle_2theta
+   logical                    :: info
 
-        type (Reflect_Type),allocatable, dimension(:) :: reflections
-        info=.true.
-        num=0
-        do
-           write(unit=*,fmt="(/,a)") " ==================================="
-           write(unit=*,fmt="(a)")   "     List of unique reflections     "
-           write(unit=*,fmt="(a)")   " ==================================="
-           write(unit=*,fmt="(a)")   " "
+   type (Reflect_Type),allocatable, dimension(:) :: reflections
+   
+   !---- Initializing ----!
+   info=.true.
+   num=0
+   
+   !---- Procedure ----!
+   do
+      write(unit=*,fmt="(/,a)") " ==================================="
+      write(unit=*,fmt="(a)")   "     List of unique reflections     "
+      write(unit=*,fmt="(a)")   " ==================================="
+      write(unit=*,fmt="(a)")   " "
 
-           if (info) then
-              write(unit=*,fmt="(a)",advance="no") " => Space Group (HM/Hall symbol or number): "
-              read(unit=*,fmt="(a)") spgr
-              if (len_trim(spgr)==0) exit
-              call set_spacegroup(spgr,grp_espacial)
-              Select Case(grp_espacial%CrystalSys)
-                case("Triclinic")
-                  write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,b,c,alpha,beta,gamma): "
-                  read(unit=*,fmt=*,iostat=ier) celda(1),celda(2),celda(3),angulo(1),angulo(2),angulo(3)
-                case("Monoclinic")
-                  write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,b,c,beta): "
-                  angulo(1)=90.0
-                  angulo(3)=90.0
-                  read(unit=*,fmt=*,iostat=ier) celda(1),celda(2),celda(3),angulo(2)
-                case("Orthorhombic")
-                  write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,b,c): "
-                  angulo(1:3)=90.0
-                  read(unit=*,fmt=*,iostat=ier) celda(1),celda(2),celda(3)
-                case("Tetragonal")
-                  write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,c): "
-                  angulo(1:3)=90.0
-                  read(unit=*,fmt=*,iostat=ier) celda(1),celda(3)
-                  celda(2)=celda(1)
-                case("Rhombohedral","Hexagonal")
-                  write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,c): "
-                  angulo(1:2)=90.0
-                  angulo(3)=120.0
-                  read(unit=*,fmt=*,iostat=ier) celda(1),celda(3)
-                  celda(2)=celda(1)
-                case("Cubic")
-                  write(unit=*,fmt="(a)",advance="no") " => Cell parameter (a): "
-                  angulo(1:3)=90.0
-                  read(unit=*,fmt=*,iostat=ier) celda(1)
-                  celda(2)=celda(1)
-                  celda(3)=celda(1)
-              End Select
-              if( ier /= 0 ) cycle
-              call set_crystal_cell(celda,angulo,cell)
-              info=.false.
-              write(unit=*,fmt="(a)",advance="no") " => Give the wavelength: "
-              read(unit=*,fmt=*) lambda
-           end if
-           write(unit=*,fmt=*) " "
-           write(unit=*,fmt="(a)",advance="no") " => Interval in Sin_Theta/Lambda (2 reals, if val1 < 0 => stops): "
-           read(unit=*,fmt=*,iostat=ier) val1,val2
-           if(ier /= 0) cycle
-           if (val1 < 0.0) exit
-           texto = "  1/Angtroms (Sin_Theta/Lambda)"
-           car="s"
+      if (info) then
+         write(unit=*,fmt="(a)",advance="no") " => Space Group (HM/Hall symbol or number): "
+         read(unit=*,fmt="(a)") spgr
+         if (len_trim(spgr)==0) exit
+         
+         !> Set the SpaceGroup Information
+         call set_spacegroup(spgr,grp_espacial)
+         
+         !> Load Cell Parameters according to the Crystal System
+         !> defined by the Space Group
+         Select Case(grp_espacial%CrystalSys)
+            case("Triclinic")
+               write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,b,c,alpha,beta,gamma): "
+               read(unit=*,fmt=*,iostat=ier) celda(1),celda(2),celda(3),angulo(1),angulo(2),angulo(3)
+         
+            case("Monoclinic")
+               write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,b,c,beta): "
+               angulo(1)=90.0
+               angulo(3)=90.0
+               read(unit=*,fmt=*,iostat=ier) celda(1),celda(2),celda(3),angulo(2)
+         
+            case("Orthorhombic")
+               write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,b,c): "
+               angulo(1:3)=90.0
+               read(unit=*,fmt=*,iostat=ier) celda(1),celda(2),celda(3)
+         
+            case("Tetragonal")
+               write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,c): "
+               angulo(1:3)=90.0
+               read(unit=*,fmt=*,iostat=ier) celda(1),celda(3)
+               celda(2)=celda(1)
+         
+            case("Rhombohedral","Hexagonal")
+               write(unit=*,fmt="(a)",advance="no") " => Cell parameters (a,c): "
+               angulo(1:2)=90.0
+               angulo(3)=120.0
+               read(unit=*,fmt=*,iostat=ier) celda(1),celda(3)
+               celda(2)=celda(1)
+         
+            case("Cubic")
+               write(unit=*,fmt="(a)",advance="no") " => Cell parameter (a): "
+               angulo(1:3)=90.0
+               read(unit=*,fmt=*,iostat=ier) celda(1)
+               celda(2)=celda(1)
+               celda(3)=celda(1)
+         End Select
+         if( ier /= 0 ) cycle
 
-           MaxNumRef = get_maxnumref(val2+0.2,Cell%CellVol,mult=grp_espacial%Multip)
-           write(unit=*,fmt="(a,i10)") " => Maximum number of reflections: ", MaxNumRef
-           if (allocated(reflections)) then
-             deallocate(reflections)
-             allocate (reflections(MaxNumRef))
-           else
-             allocate (reflections(MaxNumRef))
-           end if
+         !> Set the Cell parameters
+         call set_crystal_cell(celda,angulo,cell)
 
-        !   call HKL_GEN(cell,grp_espacial,.true.,val1,val2,num,reflections) !Not ordered
-           call HKL_UNI(cell,grp_espacial,.true.,val1,val2,car,num,reflections) !Ordered
+         !> Wavelength
+         info=.false.
+         write(unit=*,fmt="(a)",advance="no") " => Give the wavelength: "
+         read(unit=*,fmt=*) lambda
+      end if
+      
+      !> Set the range for HKL calculation
+      write(unit=*,fmt=*) " "
+      write(unit=*,fmt="(a)",advance="no") " => Interval in Sin_Theta/Lambda (2 reals, if val1 < 0 => stops): "
+      read(unit=*,fmt=*,iostat=ier) val1,val2
+      if(ier /= 0) cycle
+      if (val1 < 0.0) exit
 
-           write(unit=*,fmt="(a)",advance="no") " => Name of the output file: "
-           read(unit=*,fmt="(a)") name_file
+      texto = "  1/Angtroms (Sin_Theta/Lambda)"
+      car="s"
 
-           open(unit=1,file=trim(name_file),status="REPLACE",action="WRITE")
-              write(unit=1,fmt="(a)")   "    =========================="
-              write(unit=1,fmt="(a)")   "    LIST OF UNIQUE REFLECTIONS"
-              write(unit=1,fmt="(a,/)") "    =========================="
-              call Write_SpaceGroup(grp_espacial,1)
-              call Write_Crystal_Cell(Cell,1)
-              write(unit=1,fmt="(/,a,2f8.4,a,/)") " => List of reflections within: ",val1,val2,texto
-           do i=1,num
-              angle_2theta=2.0*asind(reflections(i)%S*lambda)
-              write(unit=1,fmt="(3i4,i5,2f10.5,i8)") reflections(i)%h, reflections(i)%mult, &
-                                                    reflections(i)%S, angle_2theta,i
-           end do
-           close(unit=1)
+      !> Allocating the vectors
+      MaxNumRef = get_maxnumref(val2+0.2,Cell%CellVol,mult=grp_espacial%Multip)
+      write(unit=*,fmt="(a,i10)") " => Maximum number of reflections: ", MaxNumRef
+      if (allocated(reflections)) then
+        deallocate(reflections)
+        allocate (reflections(MaxNumRef))
+      else
+        allocate (reflections(MaxNumRef))
+      end if
 
-        end do
-        write(unit=*,fmt="(a)") " => Program finished ... "
-    stop
-  End Program Test_HKL_GEN
+      !> Procedure to calculation the all reflections
+      !call HKL_GEN(cell,grp_espacial,.true.,val1,val2,num,reflections) !Not ordered
+      call HKL_UNI(cell,grp_espacial,.true.,val1,val2,car,num,reflections) !Ordered
+
+      !> Output Information
+      write(unit=*,fmt="(a)",advance="no") " => Name of the output file: "
+      read(unit=*,fmt="(a)") name_file
+
+      open(unit=1,file=trim(name_file),status="REPLACE",action="WRITE")
+      write(unit=1,fmt="(a)")   "    =========================="
+      write(unit=1,fmt="(a)")   "    LIST OF UNIQUE REFLECTIONS"
+      write(unit=1,fmt="(a,/)") "    =========================="
+      
+      !> Write the SpaceGroup information
+      call Write_SpaceGroup(grp_espacial,1)
+      
+      !> Write the Cell parameters information
+      call Write_Crystal_Cell(Cell,1)
+      
+      !> Write the Reflections
+      write(unit=1,fmt="(/,a,2f8.4,a,/)") " => List of reflections within: ",val1,val2,texto
+      do i=1,num
+         angle_2theta=2.0*asind(reflections(i)%S*lambda)
+         write(unit=1,fmt="(3i4,i5,2f10.5,i8)") reflections(i)%h, reflections(i)%mult, &
+                                                reflections(i)%S, angle_2theta,i
+      end do
+      close(unit=1)
+   end do
+
+   write(unit=*,fmt="(a)") " => Program finished ... "
+End Program Test_HKL_GEN
--- Program_Examples/hkl_gen/make_hkl_gen.bat
+++ Program_Examples/hkl_gen/make_hkl_gen.bat
@@ -1,4 +1,50 @@
-del *.obj
-del *.mod
-lf95 -c hkl_gen.f90    -info  -o1 -chk -mod ".;C:\crysFML\lahey\libC"
-lf95 *.obj -out hkl_gen  -o1 -mod ".;C:\crysFML\lahey\LibC" -lib C:\crysFML\lahey\libC\crysFML  -chk
+@echo off
+rem ****
+rem ****---- Compilation for HKL_GEN Program ----****
+rem ****
+rem **** Author: JRC + JGP
+rem **** Revision: Nov-2008
+rem ****
+rem
+   if not x%1 == x goto CONT
+   cls
+   echo    MAKE_HKL_GEN: Make HKL_GEN Compilation
+   echo    Syntax: make_hkl_gen [f95/lf95/g95/gfrotran/ifort]
+   goto END
+rem
+:CONT
+   if x%1 == xf95       goto F95
+   if x%1 == xlf95      goto LF95
+   if x%1 == xg95       goto G95
+   if x%1 == xgfortran  goto GFOR
+   if x%1 == xifort     goto IFORT
+   goto END
+rem
+rem ****---- Absoft Compiler ----****
+:F95
+   goto END
+rem
+rem ****---- Lahey Compiler ----****
+:LF95
+   lf95 -c hkl_gen.f90    -info  -o1 -chk -mod ".;C:\crysFML\lahey\libC"
+   lf95 *.obj -out hkl_gen  -o1 -mod ".;C:\crysFML\lahey\LibC" -lib C:\crysFML\lahey\libC\crysFML 
+   goto END
+rem      
+rem ****---- Intel Compiler ----****
+:IFORT
+   ifort /c hkl_gen.f90 /O2 /nologo /IC:\CrysFML\Intel\LibC
+   ifort /exe:hkl_gen *.obj C:\CrysFML\Intel\LibC\crysfml.lib
+   goto END
+rem
+rem **---- G95 Compiler ----**
+:G95
+   goto END
+rem
+rem **---- GFORTRAN Compiler ----**
+:GFOR
+   goto END
+rem
+   del *.obj *.o *.mod *.exe *.map
+rem
+:END
+   del *.obj *.mod *.o *.map *.bak > nul
