9f27559abb179b51e85eff49984f67a38bfac3d6
Javier Gonzalez-Platas <jplatas@ull.edu.es>
2008-11-14 18:32:02 +0000

    Update the scripts files from Optimization example
    git-svn-id: https://forge.ill.fr/svn/crysfml@101 06508225-4b59-0410-a2a9-fdab55894d57

--- Program_Examples/Structures_GlobalOptimization/make_opt_Intel.bat
+++
@@ -1,5 +0,0 @@
-del *.obj
-ifort observ.f90              /c /O3 /nologo /IC:\CrysFML\Intel\LibC
-ifort cost_functions.f90      /c /O3 /nologo /IC:\CrysFML\Intel\LibC
-ifort Optim_General.f90       /c /O3 /nologo /IC:\CrysFML\Intel\LibC
-ifort  /exe:Optim_General *.obj C:\CrysFML\Intel\LibC\CrysFML.lib /link /stack:64000000
---
+++ Program_Examples/Structures_GlobalOptimization/make_optim_gen.bat
@@ -0,0 +1,52 @@
+@echo off
+rem ****
+rem ****---- Compilation for OPTIM_GEN Program ----****
+rem ****
+rem **** Author: JRC + JGP
+rem **** Revision: Nov-2008
+rem ****
+rem
+   if not x%1 == x goto CONT
+   cls
+   echo    MAKE_OPTIM_GEN: Make OPTIM_GEN Compilation
+   echo    Syntax: make_optim_gen [f95/lf95/g95/gfrotran/ifort]
+   goto END
+rem
+:CONT
+   if x%1 == xf95       goto F95
+   if x%1 == xlf95      goto LF95
+   if x%1 == xg95       goto G95
+   if x%1 == xgfortran  goto GFOR
+   if x%1 == xifort     goto IFORT
+   goto END
+rem
+rem ****---- Absoft Compiler ----****
+:F95
+   goto END
+rem
+rem ****---- Lahey Compiler ----****
+:LF95
+   lf95 -c observ.f90             -tp  -stchk -chk -o0 -mod ".;c:\CrysFML\Lahey\LibC" -lib c:\CrysFML\Lahey\LibC\CrysFML
+   lf95 -c cost_functions.f90     -tp  -stchk -chk -o0 -mod ".;c:\CrysFML\Lahey\LibC" -lib c:\CrysFML\Lahey\LibC\CrysFML
+   lf95 -c Optim_General.f90      -tp  -stchk -chk -o0 -mod ".;c:\CrysFML\Lahey\LibC" -lib c:\CrysFML\Lahey\LibC\CrysFML
+   lf95  *.obj -out Optim_General -tp  -stchk -chk -o0 -mod ".;c:\CrysFML\Lahey\LibC" -lib c:\CrysFML\Lahey\LibC\CrysFML
+   goto END
+rem      
+rem ****---- Intel Compiler ----****
+:IFORT
+   ifort /c observ.f90             /O2 /nologo /IC:\CrysFML\Intel\LibC
+   ifort /c cost_functions.f90     /O2 /nologo /IC:\CrysFML\Intel\LibC
+   ifort /c Optim_General.f90      /O2 /nologo /IC:\CrysFML\Intel\LibC
+   ifort /exe:Optim_General *.obj  C:\CrysFML\Intel\LibC\CrysFML.lib /link /stack:64000000
+   goto END
+rem
+rem **---- G95 Compiler ----**
+:G95
+   goto END
+rem
+rem **---- GFORTRAN Compiler ----**
+:GFOR
+   goto END
+rem
+:END
+   del *.obj *.mod *.o *.map *.bak > nul
