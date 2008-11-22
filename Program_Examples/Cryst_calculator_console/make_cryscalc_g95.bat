8727c42f7a757e017e7125f28636b50373e0ad94
Juan Rodriguez-Carvajal <jrc@ill.fr>
2008-11-22 16:51:33 +0000

    git-svn-id: https://forge.ill.fr/svn/crysfml@113 06508225-4b59-0410-a2a9-fdab55894d57

--- Program_Examples/Cryst_calculator_console/make_cryscalc_g95.bat
+++
@@ -1,7 +0,0 @@
-g95 -c -O3    -funroll-loops  -msse2   menu_1.f90     -IC:\CrysFML\G95\LibC
-g95 -c -O3    -funroll-loops  -msse2   menu_2.f90     -IC:\CrysFML\G95\LibC
-g95 -c -O3    -funroll-loops  -msse2   menu_3.f90     -IC:\CrysFML\G95\LibC
-g95 -c -O3    -funroll-loops  -msse2   menu_4.f90     -IC:\CrysFML\G95\LibC
-g95 -c -O3    -funroll-loops  -msse2   calsym.f90     -IC:\CrysFML\G95\LibC
-g95  *.o -o cryscalc -O3  -funroll-loops  -msse2  -LC:\CrysFML\G95\LibC -lcrysfml
-del *.o *.mod *.bak *.map
