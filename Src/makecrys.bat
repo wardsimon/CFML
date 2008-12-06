902dd0c7c8148feabf66d81e1a8639b450020b6f
Javier Gonzalez-Platas <jplatas@ull.edu.es>
2008-12-06 21:43:09 +0000

    Updating Help on CrysFML
    git-svn-id: https://forge.ill.fr/svn/crysfml@127 06508225-4b59-0410-a2a9-fdab55894d57

---
+++ Src/makecrys.bat
@@ -0,0 +1,25 @@
+@echo off
+rem
+rem Crystallographic Fortran Modules Library
+rem Author: JGP
+rem Revision: Dic-2009
+rem
+   if not x%1 == x goto CONT
+rem
+   cls
+   echo MAKECRYS: Making the CrysFML and CrysFGL Libraries
+   echo Syntax: makecrys [f95/lf95/g95/ifort/gfortran] [con,all]
+   goto FINAL
+rem
+:CONT
+   cd .\scripts\windows
+   call make_crysfml %1 
+   if x%2 == xall goto ALL
+   goto END
+:ALL   
+   call make_crysfml %1 win
+   call make_crysfgl %1
+:END   
+   cd ..\..\..
+rem
+:FINAL
