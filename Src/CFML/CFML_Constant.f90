5cfbd8e8d75ce73fa4558398f6ab076502f02a81
Juan Rodriguez-Carvajal <jrc@ill.fr>
2009-03-06 09:04:24 +0000

    Adding Operating System variables in CFML_Constant.f90
    git-svn-id: https://forge.ill.fr/svn/crysfml@166 06508225-4b59-0410-a2a9-fdab55894d57

--- Src/CFML/CFML_Constant.f90
+++ Src/CFML/CFML_Constant.f90
@@ -10,6 +10,11 @@
 !!--..
 !!---- VARIABLES
 !!--..
+!!--..    Operating system
+!!--..
+!!----    OPS
+!!----    OPS_NAME
+!!--..
 !!--..    Precision Data
 !!--..
 !!----    SP
@@ -33,23 +38,47 @@ Module CFML_Constants
    !---- Variables ----!
    implicit None
 
+   !------------------------------------!
+   !---- Operating System variables ----!
+   !------------------------------------!
+
+   !!----
+   !!---- OPS
+   !!----   Integer variable 1: Windows, 2: Mac, 3: Linux
+   !!----
+   !!---- Update: March 2009
+   !!
+   integer, parameter :: OPS= 1
+   !integer, parameter :: OPS= 2
+   !integer, parameter :: OPS= 3
+
+   !!----
+   !!---- OPS_NAME
+   !!----   Character variable containing the name of the operating system
+   !!----
+   !!---- Update: March 2009
+   !!
+   character(len=*), parameter :: OPS_NAME="Windows"
+   !character(len=*), parameter :: OPS_NAME="MacOS"
+   !character(len=*), parameter :: OPS_NAME="Linux"
+
    !------------------------------!
    !---- Precision Parameters ----!
    !------------------------------!
-   
+
    !!----
    !!---- SP
    !!----    SP: Single precision ( sp = selected_real_kind(6,30) )
    !!----
-   !!---- Update: January - 2009 
+   !!---- Update: January - 2009
    !!
    integer, parameter :: sp = selected_real_kind(6,30)
-   
+
    !!----
    !!---- DP
    !!----    DP: Double precision ( dp = selected_real_kind(14,150) )
    !!----
-   !!---- Update: January - 2009 
+   !!---- Update: January - 2009
    !!
    integer, parameter :: dp = selected_real_kind(14,150)
 
@@ -57,10 +86,10 @@ Module CFML_Constants
    !!---- CP
    !!----    CP: Current precision
    !!----
-   !!---- Update: January - 2009 
+   !!---- Update: January - 2009
    !!
    integer, parameter :: cp = sp
-   
+
    !----------------------------------!
    !---- Trigonometric Parameters ----!
    !----------------------------------!
@@ -71,7 +100,7 @@ Module CFML_Constants
    !!----
    !!----    Pi value
    !!----
-   !!---- Update: January - 2009 
+   !!---- Update: January - 2009
    !!
    real(kind=dp), parameter ::  pi = 3.141592653589793238463_dp
 
@@ -81,7 +110,7 @@ Module CFML_Constants
    !!----
    !!----    Conversion from Rad to Degree
    !!----
-   !!---- Update: January - 2009 
+   !!---- Update: January - 2009
    !!
    real(kind=dp), parameter ::  to_DEG  = 180.0_dp/pi
 
@@ -91,7 +120,7 @@ Module CFML_Constants
    !!----
    !!----    Conversion from Degree to Rad
    !!----
-   !!---- Update: January - 2009 
+   !!---- Update: January - 2009
    !!
    real(kind=dp), parameter ::  to_RAD  = pi/180.0_dp
 
@@ -101,21 +130,21 @@ Module CFML_Constants
    !!----
    !!----  2.0*Pi value
    !!----
-   !!---- Update: January - 2009 
+   !!---- Update: January - 2009
    !!
-   real(kind=dp), parameter ::  tpi = 6.283185307179586476925_dp   
-   
+   real(kind=dp), parameter ::  tpi = 6.283185307179586476925_dp
+
    !----------------------------!
    !---- Numeric Parameters ----!
    !----------------------------!
-   
+
    !!----
    !!---- DEPS
    !!----    real(kind=dp), parameter :: deps=0.00000001_dp
    !!----
    !!----    Epsilon value
    !!----
-   !!---- Update: January - 2009 
+   !!---- Update: January - 2009
    !!
    real(kind=dp), parameter, public :: deps=0.00000001_dp
 
@@ -125,8 +154,8 @@ Module CFML_Constants
    !!----
    !!----     Epsilon value
    !!----
-   !!----  Update: January - 2009 
+   !!----  Update: January - 2009
    !!
    real(kind=cp),  parameter, public  ::  eps=0.00001_cp
-   
+
 End Module CFML_Constants
