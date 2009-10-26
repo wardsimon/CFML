b696b5e078693e813495c6156f1ffd3b39bf25f2
Juan Rodriguez-Carvajal <jrc@ill.fr>
2009-10-26 17:53:55 +0000

    Change in script files for taking into account the new name of CFML_GlobalDeps_xxx.f90 files. This is only valid for Linux and MacOS, the Windows scrips make reference to the default CFML_GlobalDeps.f90. A new item has been introduced in makecrys.bat, in order to compile the library for RealWin (Lahey-Fujitsu), without compiling for Winteracter.
    git-svn-id: https://forge.ill.fr/svn/crysfml@259 06508225-4b59-0410-a2a9-fdab55894d57

--- Src/makecrys.bat
+++ Src/makecrys.bat
@@ -8,18 +8,22 @@ rem
 rem
    cls
    echo MAKECRYS: Making the CrysFML and CrysFGL Libraries
-   echo Syntax: makecrys [f95/lf95/g95/ifort/gfortran] [con,all]
+   echo Syntax: makecrys [f95/lf95/g95/ifort/gfortran] [con,all,rw]
    goto FINAL
 rem
 :CONT
    cd .\scripts\windows
    call make_crysfml %1
    if x%2 == xall goto ALL
+   if x%2 == xrw  goto RW
    goto END
 :ALL
    if x%1 == xlf95 call make_crysfml %1 rwin
    call make_crysfml %1 win
    call make_crysfgl %1
+   goto END
+:RW
+   if x%1 == xlf95 call make_crysfml %1 rwin
 :END
    cd ..\..
 rem
