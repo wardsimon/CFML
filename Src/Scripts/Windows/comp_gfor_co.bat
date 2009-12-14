1dd256deeb4cae07d42e9b17ea9b0ad425370e89
Juan Rodriguez-Carvajal <jrc@ill.fr>
2009-12-14 17:31:43 +0000

    Small change in the Windows script for gfortran
    git-svn-id: https://forge.ill.fr/svn/crysfml@265 06508225-4b59-0410-a2a9-fdab55894d57

--- Src/Scripts/Windows/comp_gfor_co.bat
+++ Src/Scripts/Windows/comp_gfor_co.bat
@@ -37,7 +37,7 @@ rem
    gfortran -c CFML_bonds_table.f90      -O0  -std=f2003  -funroll-loops  -msse2  >> out
    gfortran -c CFML_cryst_types.f90      -O3  -std=f2003  -funroll-loops  -msse2  >> out
    gfortran -c CFML_symmetry.f90         -O3  -std=f2003  -funroll-loops  -msse2  >> out
-   gfortran -c CFML_ILL_Instrm_data.f90  -O3  -std=f2003  -funroll-loops  -msse2  >> out
+   gfortran -c CFML_ILL_Instrm_data.f90  -O3  -std=gnu    -funroll-loops  -msse2  >> out
 rem
    echo **---- Level 3 ----**
    echo .... Reflections, Atoms, Polarimetry
