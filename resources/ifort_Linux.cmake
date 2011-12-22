fe28f8166871a3fd475f1a1c1fc310e65aed1ede
Eric Pellegrini <pellegrini@ill.fr>
2011-12-22 12:50:08 +0000

    Handled the case of 32/64 bit winteracter library paths
    git-svn-id: https://forge.ill.fr/svn/crysfml@525 06508225-4b59-0410-a2a9-fdab55894d57

--- resources/ifort_Linux.cmake
+++ resources/ifort_Linux.cmake
@@ -2,6 +2,10 @@ set(CMAKE_Fortran_FLAGS_DEBUG "-g -warn")
 
 set(CMAKE_Fortran_FLAGS_RELEASE "-O -warn -vec-report0")
 
-set(WINTER_MOD_DIR $ENV{WINTER}/lib.if8)
-
-set(WINTER_LIB_DIR $ENV{WINTER}/lib.if8)
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(WINTER_MOD_DIR $ENV{WINTER}/lib.i64)
+    set(WINTER_LIB_DIR $ENV{WINTER}/lib.i64)
+else()
+    set(WINTER_MOD_DIR $ENV{WINTER}/lib.if8)
+    set(WINTER_LIB_DIR $ENV{WINTER}/lib.if8)
+endif()
