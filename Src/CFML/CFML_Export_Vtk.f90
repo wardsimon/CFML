81fee2c5de543aa4f9348514bcc58cb96f00482a
Laurent Chapon <chapon@ill.fr>
2011-12-25 14:00:19 +0000

    First working version to export vtk structured grid (vts) file
    git-svn-id: https://forge.ill.fr/svn/crysfml@531 06508225-4b59-0410-a2a9-fdab55894d57

--- Src/CFML/CFML_Export_Vtk.f90
+++ Src/CFML/CFML_Export_Vtk.f90
@@ -12,12 +12,16 @@
 !!----
 Module CFML_Export_VTK
 
-use CFML_Globaldeps, Only: cp, dp
+use CFML_Globaldeps, Only: dp, dp
 use CFML_Crystal_metrics, Only: Crystal_Cell_Type
+use CFML_String_Utilities, Only: Pack_String
 
 implicit none
 
+character(1), parameter:: end_line=char(10)
+
 contains
+
 !!----
 !!---- Subroutine array3D_2_vts(Z,filename)
 !!----    real(kind=dp), dimension(:,:,:), intent(in) :: Z			! 3D Array to export
@@ -31,14 +35,14 @@ contains
 !!---- Update: December - 2011
 !!
 subroutine array3D_pointdata_2_vts(Z,cell,xmin,xmax,ymin,ymax,zmin,zmax,filename)
-	real(kind=cp), dimension(:,:,:), intent(in) :: Z			! 3D Array to export
+	real(kind=dp), dimension(:,:,:), intent(in) :: Z      ! 3D Array to export
 	type(Crystal_Cell_Type), intent(in)         :: cell   ! The unit-cell
-	real(kind=cp), intent(in)                   :: xmin   ! Limits of x
-	real(kind=cp), intent(in)                   :: xmax   ! Limits of x
-	real(kind=cp), intent(in)                   :: ymin   ! Limits of y
-	real(kind=cp), intent(in)                   :: ymax   ! Limits of y
-	real(kind=cp), intent(in)                   :: zmin   ! Limits of z
-	real(kind=cp), intent(in)                   :: zmax   ! Limits of z
+	real(kind=dp), intent(in)                   :: xmin   ! Limits of x
+	real(kind=dp), intent(in)                   :: xmax   ! Limits of x
+	real(kind=dp), intent(in)                   :: ymin   ! Limits of y
+	real(kind=dp), intent(in)                   :: ymax   ! Limits of y
+	real(kind=dp), intent(in)                   :: zmin   ! Limits of z
+	real(kind=dp), intent(in)                   :: zmax   ! Limits of z
 	Character(len=*),                intent(in) :: filename ! Name of the vtk file
 	! Local variables
 	integer :: nx, ny, nz
@@ -46,60 +50,101 @@ subroutine array3D_pointdata_2_vts(Z,cell,xmin,xmax,ymin,ymax,zmin,zmax,filename
 	logical :: i_error
 	integer :: i,j,k
 	integer :: nbytes, nbits
-	character(len=256) :: tpc, tamere
+	character(len=48)   :: nbit_c, offset_c, nx_c, ny_c, nz_c
+	character(len=1000) :: header
 	integer :: offset
-	real(kind=cp) ,dimension(3) :: pos
+	real(kind=dp), dimension(3) :: pos
+    integer :: ntot ! Total number of points
+    integer :: ntot_bytes
 
-	! Check that the file is valid
-	open(unit=vtk_id,file=trim(filename)//'.vts',form='unformatted',status='unknown',iostat=ier)
-	if (ier/=0) then
+
+	! Open the file and check that the file is valid
+	open(unit=vtk_id,file=trim(filename)//'.vts', &
+         form       = 'UNFORMATTED',  &
+         access     = 'SEQUENTIAL',   &
+         action     = 'WRITE',        &
+         convert    = 'LITTLE_ENDIAN',&
+         recordtype = 'STREAM',       &
+         buffered   = 'YES',          &
+         iostat=ier)
+	 if (ier/=0) then
 		i_error=.true.
-	end if
-	! Check the extent of the array (C style 0:n-1)
-	nx=size(Z,1)-1
-	ny=size(Z,2)-1
-	nz=size(Z,3)-1
-	! Determine the number of bits used
-	nbytes=sizeof(Z(1,1,1))
-	nbits=8*nbytes
-	write(tpc,*) nbits
-	! Calculate the offset between points and Z
-	offset=nbytes*(nx+1)*(ny+1)*(nz+1)+4
-	write(tamere,*) offset
-	! Write the header
-  write(vtk_id) trim('<?xml version="1.0"?>')
-	write(vtk_id) trim('<VTKFile byte_order="LittleEndian" version="0.1" type="StructuredGrid"> \n')
-	write(vtk_id) '<StructuredGrid WholeExtent="', 0, nx, 0, ny, 0, nz, '">'
-	write(vtk_id) '<Piece Extent="', 0, nx, 0, ny, 0, nz,'">'
-	! Points represent the
-	write(vtk_id) '<Points>'
-	write(vtk_id) '<DataArray NumberOfComponents="3" offset="0" type="Float'//trim(tpc)//'" Name="points" format="appended"/>'
-	write(vtk_id) '</Points>'
-	write(vtk_id) '<PointData scalars="density">'
-	write(vtk_id) '<DataArray NumberOfComponents="1" offset="'//trim(tamere)//'" type="Float'//trim(tpc)//'" Name="density" format="appended"/>'
-	write(vtk_id) '</PointData>'
-	write(vtk_id) '</Piece>'
-	write(vtk_id) '</StructuredGrid>'
-  !
-	write(vtk_id) '<AppendedData encoding="raw">'
-	! This is where the data is written
-	do i=0,nx
-		do j=0,ny
-			do k=0,nz
-				pos=(/xmin+(xmax-xmin)*i/nx,ymin+(ymax-ymin)*j/ny,zmin+(zmax-zmin)*k/nz/)
-				pos=Matmul(cell%Cr_Orth_cel,pos)
-				write(vtk_id) pos
-				write(vtk_id) Z(i,j,k)
-			end do
-		end do
-	end do
-	! Finished to write the data, close the xml
-	write(vtk_id) '</AppendedData>'
-  write(vtk_id) '</VTKFile>'
+	 end if
+
+	 ! Get the extent in each direction. Will be written as C array [0,n-1]
+	 nx=size(Z,1)-1
+     ny=size(Z,2)-1
+     nz=size(Z,3)-1
+
+     ! Total number of points
+     ntot=(nx+1)*(ny+1)*(nz+1)
+
+     ! Determine the number of bits used
+     nbytes=sizeof(Z(1,1,1))
+     nbits=8*nbytes
+     !
+     ntot_bytes=ntot*nbytes*3
+     ! Calculate the offset between points and Z
+     offset=nbytes*ntot*3+4
+     !
+     ! ----------- Here starts the header
+	 write(unit=vtk_id) '<?xml version="1.0"?>'//end_line
+     write(unit=vtk_id) '<VTKFile byte_order="LittleEndian" version="0.1" type="StructuredGrid">'//end_line
+     write(unit=nx_c,fmt=*) nx
+     write(unit=ny_c,fmt=*) ny
+     write(unit=nz_c,fmt=*) nz
+     write(unit=header,fmt='(A)') '<StructuredGrid WholeExtent="0 '//trim(Pack_String(nx_c))//' 0 '//trim(Pack_String(ny_c))//' 0 '//trim(Pack_String(nz_c))//'">'
+     write(unit=vtk_id) trim(header)//end_line
+     write(unit=header,fmt='(A)') '<Piece Extent="0 '//trim(Pack_String(nx_c))//' 0 '//trim(Pack_String(ny_c))//' 0 '//trim(Pack_String(nx_c))//'">'
+     write(unit=vtk_id) trim(header)//end_line
+     write(unit=vtk_id) '<Points>'//end_line
+     write(unit=nbit_c,fmt=*) nbits
+     write(unit=header,fmt='(A)') '<DataArray NumberOfComponents="3" offset="0" type="Float'//trim(Pack_String(nbit_c))//'" Name="points" format="appended"/>'
+     write(unit=vtk_id) trim(header)//end_line
+     write(unit=vtk_id) '</Points>'//end_line
+     write(unit=vtk_id) '<PointData scalars="density">'//end_line
+     write(unit=offset_c,fmt=*) offset
+     write(unit=header,fmt='(A)') '<DataArray NumberOfComponents="1" offset="'//trim(Pack_string(offset_c))//'" type="Float'//trim(Pack_String(nbit_c))//'" Name="density" format="appended"/>'
+     write(unit=vtk_id) trim(header)//end_line
+     write(unit=vtk_id) '</PointData>'//end_line
+     write(unit=vtk_id) '</Piece>'//end_line
+     write(unit=vtk_id) '</StructuredGrid>'//end_line
+     write(unit=vtk_id) '<AppendedData encoding="raw">'//end_line
+     write(unit=vtk_id) '_'
+    !------------- Here finishes the header
+    ! This is where the data is written
+
+    ! First we write the number of bytes for the points
+     write(unit=vtk_id) ntot*nbytes*3
+    ! Then write the coordinates of the points
+	 do i=0,nx
+        do j=0,ny
+            do k=0,nz
+                pos=(/xmin+(xmax-xmin)*i/nx,ymin+(ymax-ymin)*j/ny,zmin+(zmax-zmin)*k/nz/)
+                pos=Matmul(cell%Cr_Orth_cel,pos)
+                write(vtk_id) pos
+            end do
+          end do
+        end do
+     ! write the number of bytes for the point data
+       write(unit=vtk_id) ntot*nbytes
+     ! write the point data
+      do i=0,nx
+        do j=0,ny
+            do k=0,nz
+                write(vtk_id) Z(i+1,j+1,k+1)
+            end do
+        end do
+     end do
+  ! Finished to write the data, close the xml
+  write(vtk_id) end_line
+  write(vtk_id) '</AppendedData>'//end_line
+  write(vtk_id) '</VTKFile>'//end_line
   ! Close the file
   close(unit=vtk_id)
 
 end subroutine array3D_pointdata_2_vts
+
 
 end Module CFML_Export_Vtk
 
