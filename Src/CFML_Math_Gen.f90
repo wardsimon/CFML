c19e3bb320588684493d81b4dc49a8b084a04c73
Juan Rodriguez-Carvajal <jrc@ill.fr>
2012-01-19 02:06:03 +0000

    Changing to public the table of the first 1000 prime numbers in CFML_Math_Gen.f90
    git-svn-id: https://forge.ill.fr/svn/crysfml@552 06508225-4b59-0410-a2a9-fdab55894d57

--- Src/CFML_Math_Gen.f90
+++ Src/CFML_Math_Gen.f90
@@ -263,14 +263,14 @@
 
     !!----
     !!---- Primes
-    !!----    integer, parameter, dimension(1000), private :: primes
+    !!----    integer, parameter, dimension(1000), public :: primes
     !!----
     !!----    List of the first 1000 prime numbers.
     !!----    Used by the subroutine Co_Prime_Vector and function Co_Prime
     !!----
     !!---- Update: January - 2011
     !!
-    integer, parameter, dimension(1000), private :: primes =                                       &
+    integer, parameter, dimension(1000), public :: primes =                                       &
            (/ 2,      3,      5,      7,     11,     13,     17,     19,     23,     29,  &
              31,     37,     41,     43,     47,     53,     59,     61,     67,     71,  &
              73,     79,     83,     89,     97,    101,    103,    107,    109,    113,  &
