5bdf4b50da10e224437fd9cd94e87cbe3eef7737
Eric Pellegrini <pellegrini@ill.fr>
2012-01-25 07:56:32 +0000

    Removed unused files
    git-svn-id: https://forge.ill.fr/svn/crysfml@569 06508225-4b59-0410-a2a9-fdab55894d57

--- cmake/FindWINTERACTER.cmake
+++ cmake/FindWINTERACTER.cmake
@@ -1,48 +1,153 @@
 include(LibFindMacros)
 
-include(set_winteracter_paths)
-set_winteracter_paths(WINTERACTER_PATHS)
-
 get_filename_component(COMPILER_NAME ${CMAKE_Fortran_COMPILER} NAME_WE)
 
-if(CMAKE_SIZEOF_VOID_P MATCHES 4)
-    set(ARCH64 False)
-else()
-    set(ARCH64 True)
-endif()
+if (WIN32)
 
-set(WINTERACTER_LIBRARY_PATHS)
-set(WINTERACTER_INCLUDE_PATHS)
-        
-foreach(f ${WINTERACTER_PATHS})
-    set(WINTERACTER_INCLUDE_PATHS ${WINTERACTER_INCLUDE_PATHS} ${f}/include)
+    find_path(WINTERACTER_INCLUDE_DIR
+              NAMES winparam.h
+              PATHS $ENV{WINTERACTER}/include
+                    $ENV{WINTER}/include
+                    $ENV{WINT}/include
+                    $ENV{USERPROFILE}/wint/include
+                    $ENV{HOMEDRIVE}/wint/include
+                    $ENV{SYSTEMDRIVE}/wint/include)
+
     if(COMPILER_NAME STREQUAL ifort)
-        if(${ARCH64})
-            set(WINTERACTER_LIBRARY_PATHS ${WINTERACTER_LIBRARY_PATHS} ${f}/lib.i64)
-        else()
-            set(WINTERACTER_LIBRARY_PATHS ${WINTERACTER_LIBRARY_PATHS} ${f}/lib.if8)
-        endif()
-    elseif(COMPILER_NAME STREQUAL g95)        
-        if(${ARCH64})
-            set(WINTERACTER_LIBRARY_PATHS ${WINTERACTER_LIBRARY_PATHS} ${f}/lib.g95)
-        else()
-            set(WINTERACTER_LIBRARY_PATHS ${WINTERACTER_LIBRARY_PATHS} ${f}/lib.g95)
-        endif()        
+
+        find_path(WINTERACTER_MOD_DIR
+                  NAMES winteracter.mod
+                  PATHS $ENV{USERPROFILE}/wint/lib.i64
+                        $ENV{USERPROFILE}/wint/lib.if8
+                        $ENV{HOMEDRIVE}/wint/lib.i64
+                        $ENV{HOMEDRIVE}/wint/lib.if8
+                        $ENV{SYSTEMDRIVE}/wint/lib.i64
+                        $ENV{SYSTEMDRIVE}/wint/lib.if8
+                        $ENV{WINTERACTER}/lib.i64
+                        $ENV{WINTERACTER}/lib.if8
+                        $ENV{WINTER}/lib.i64
+                        $ENV{WINTER}/lib.if8
+                        $ENV{WINT}/lib.i64
+                        $ENV{WINT}/lib.if8)
+
+        find_library(WINTERACTER_LIBRARY
+                     NAMES winter wint
+                     PATHS $ENV{WINTERACTER}/lib.i64
+                           $ENV{WINTERACTER}/lib.if8
+                           $ENV{WINTER}/lib.i64
+                           $ENV{WINTER}/lib.if8
+                           $ENV{WINT}/lib.i64
+                           $ENV{WINT}/lib.if8
+                           $ENV{USERPROFILE}/wint/lib.i64
+                           $ENV{USERPROFILE}/wint/lib.if8
+                           $ENV{HOMEDRIVE}/wint/lib.i64
+                           $ENV{HOMEDRIVE}/wint/lib.if8
+                           $ENV{SYSTEMDRIVE}/wint/lib.i64
+                           $ENV{SYSTEMDRIVE}/wint/lib.if8)
+    
+    elseif(COMPILER_NAME STREQUAL g95)
+
+        find_path(WINTERACTER_MOD_DIR
+                  NAMES winteracter.mod
+                  PATHS $ENV{WINTERACTER}/lib.g95
+                        $ENV{WINTER}/lib.g95
+                        $ENV{WINT}/lib.g95
+                        $ENV{USERPROFILE}/wint/lib.g95
+                        $ENV{HOMEDRIVE}/wint/lib.g95
+                        $ENV{SYSTEMDRIVE}/wint/lib.g95)
+
+        find_library(WINTERACTER_LIBRARY
+                     NAMES winter wint
+                     PATHS $ENV{WINTERACTER}/lib.g95
+                           $ENV{WINTER}/lib.g95
+                           $ENV{WINT}/lib.g95
+                           $ENV{USERPROFILE}/wint/lib.g95
+                           $ENV{HOMEDRIVE}/wint/lib.g95
+                           $ENV{SYSTEMDRIVE}/wint/lib.g95)
+    
+    endif()        
+    
+else()
+
+    find_path(WINTERACTER_INCLUDE_DIR
+              NAMES winparam.h
+              PATHS $ENV{WINTERACTER}/include
+                    $ENV{WINTER}/include
+                    $ENV{WINT}/include
+                    $ENV{HOME}/wint/include
+                    $ENV{HOME}/include
+                    /usr/local/lib/wint/include
+                    /usr/lib/wint/include
+                    /opt/lib/wint/include)
+
+    if(COMPILER_NAME STREQUAL ifort)
+
+        find_path(WINTERACTER_MOD_DIR
+                  NAMES winteracter.mod
+                  PATHS $ENV{WINTERACTER}/lib.i64
+                        $ENV{WINTERACTER}/lib.if8                  
+                        $ENV{WINTER}/lib.i64
+                        $ENV{WINTER}/lib.if8
+                        $ENV{WINT}/lib.i64
+                        $ENV{WINT}/lib.if8
+                        $ENV{HOME}/wint/lib.i64
+                        $ENV{HOME}/wint/lib.if8
+                        $ENV{HOME}/lib.i64
+                        $ENV{HOME}/lib.if8
+                        /usr/local/lib/wint/lib.i64
+                        /usr/local/lib/wint/lib.if8
+                        /usr/lib/wint/lib.i64
+                        /usr/lib/wint/lib.if8
+                        /opt/lib/wint/lib.i64
+                        /opt/lib/wint/lib.if8)
+
+        find_library(WINTERACTER_LIBRARY
+                     NAMES winter wint
+                     PATHS $ENV{WINTERACTER}/lib.i64
+                           $ENV{WINTERACTER}/lib.if8                  
+                           $ENV{WINTER}/lib.i64
+                           $ENV{WINTER}/lib.if8
+                           $ENV{WINT}/lib.i64
+                           $ENV{WINT}/lib.if8
+                           $ENV{HOME}/wint/lib.i64
+                           $ENV{HOME}/wint/lib.if8
+                           $ENV{HOME}/lib.i64
+                           $ENV{HOME}/lib.if8
+                           /usr/local/lib/wint/lib.i64
+                           /usr/local/lib/wint/lib.if8
+                           /usr/lib/wint/lib.i64
+                           /usr/lib/wint/lib.if8
+                           /opt/lib/wint/lib.i64
+                           /opt/lib/wint/lib.if8)
+
+    elseif(COMPILER_NAME STREQUAL g95)
+
+        find_path(WINTERACTER_MOD_DIR
+                  NAMES winteracter.mod
+                  PATHS $ENV{WINTERACTER}/lib.g95
+                        $ENV{WINTER}/lib.g95
+                        $ENV{WINT}/lib.g95
+                        $ENV{HOME}/wint/lib.g95
+                        $ENV{HOME}/lib.g95
+                        /usr/local/lib/wint/lib.g95
+                        /usr/lib/wint/lib.g95
+                        /opt/lib/wint/lib.g95)
+
+        find_library(WINTERACTER_LIBRARY
+                     NAMES winter wint
+                     PATHS $ENV{WINTERACTER}/lib.g95
+                           $ENV{WINTER}/lib.g95
+                           $ENV{WINT}/lib.g95
+                           $ENV{HOME}/wint/lib.g95
+                           $ENV{HOME}/lib.g95
+                           /usr/local/lib/wint/lib.g95
+                           /usr/lib/wint/lib.g95
+                           /opt/lib/wint/lib.g95)
+    
     endif()
-endforeach()
-                
-find_path(WINTERACTER_INCLUDE_DIR
-          NAMES winparam.h
-          PATHS ${WINTERACTER_INCLUDE_PATHS})
 
-find_path(WINTERACTER_MOD_DIR
-          NAMES winteracter.mod
-          PATHS ${WINTERACTER_LIBRARY_PATHS})
-
-find_library(WINTERACTER_LIBRARY
-             NAMES winter wint
-             PATHS ${WINTERACTER_LIBRARY_PATHS})
-                          
+endif()
+                                                  
 set(WINTERACTER_PROCESS_INCLUDES WINTERACTER_INCLUDE_DIR)
 
 set(WINTERACTER_PROCESS_MODS WINTERACTER_MOD_DIR)
