cf2946e9a4cc37d37667e3702267b41b04592d6f
Juan Rodriguez-Carvajal <jrc@ill.fr>
2012-07-04 05:51:45 +0000

    New changes in CFML_Polar.f90 for including the nutator and precesion coil angles for the incident polarisation in calculated the scattered polarisation and the polarisation tensor.
    git-svn-id: https://forge.ill.fr/svn/crysfml@641 06508225-4b59-0410-a2a9-fdab55894d57

--- Src/CFML_Polar.f90
+++ Src/CFML_Polar.f90
@@ -72,9 +72,10 @@
     Use CFML_GlobalDeps,                 only: cp, tpi
     Use CFML_Crystal_Metrics,            only: Set_Crystal_Cell, Crystal_Cell_type, Cart_Vector
     Use CFML_Math_3D,                    only: Cross_Product,Tensor_Product,Mat_Cross,Invert_A
+    Use CFML_Math_General,               only: atan2d,sind,cosd
     use CFML_Magnetic_Structure_Factors, only: MagHD_Type
     use CFML_Magnetic_Symmetry,          only: Magnetic_domain_type
-    use CFML_Geometry_SXTAL,             only: Phi_mat,Get_Angs_NB
+    use CFML_Geometry_SXTAL,             only: Phi_mat,Chi_mat, Psi_mat,Get_Angs_NB
 
     !---- Variables ----!
     implicit none
@@ -614,10 +615,10 @@
     !!----
     !!---- Subroutine Calc_Polar(frame,wave,Cell, UB, Pin, NSF, Mag_dom, Mh, Pf,ok,mess,B_Q)
     !!----    real(kind=cp),                intent(in) :: wave    ! In -> Cell variable
-    !!----    character(len=2),             intent(in) :: frame   ! In -> Scattering vector as (hkl)
+    !!----    character(len=3),             intent(in) :: frame   ! In -> Frame used for polarisation
     !!----    type (Crystal_Cell_Type),     intent(in) :: Cell    ! In -> Busing-Levy UB-matrix
     !!----    Real(kind=cp), dimension(3,3),intent(in) :: UB      ! In -> Nuclear Structure Factor
-    !!----    Real(kind=cp), dimension(3),  intent(in) :: Pin     ! In -> Incident polarisation
+    !!----    Real(kind=cp), dimension(3),  intent(in) :: Pin     ! In -> Incident polarisation (Cartesian or spherical)
     !!----    complex,                      intent(in) :: NSF     ! In -> Nuclear Structure Factor
     !!----    type(Magnetic_Domain_type),   intent(in) :: Mag_Dom ! In -> Magnetic domains information
     !!----    type(MagHD_Type),             intent(in) :: Mh      ! In -> Contains Magnetic structure factor, MiV, domain info, ...
@@ -631,9 +632,18 @@
     !!----    So that the equations written below have the sign of the terms containing explicitly
     !!----    the imaginary unit opposite to the terms of the original equations.
     !!----    This soubroutine calculates the final polarization in two reference frames: BL and BM
-    !!----    BL-> Busing-Levy and BM-> Blume-Maleyev. Calling N the nuclear structure factor
-    !!----    (complex scalar) and M* the magnetic interaction vector (complex vector), the B-M
-    !!----    equations are:
+    !!----    BL-> Busing-Levy and BM-> Blume-Maleyev. The incident polarisation can be provided
+    !!----    in Cartesian components or giving the module and two angles of rotation (Theta,Chi)
+    !!----    corresponding to nutator and precesion coils in MAD, by default they are in Cartesian
+    !!----    when frame="BL" or frame="BM". The Theta rotation corresponds to a clockwise
+    !!----    rotation around the "yBL-axis" and the Chi rotation to a clockwise rotation
+    !!----    around the "xBL-axis). The product of the two matrices R_chi.R_theta applied to (0,0,pol)
+    !!----    provides the incident polarisation in the BL system.
+    !!----    For providing "angular coordinates" frame="BLS", frame="BMS" or
+    !!----    frame="MAD" (BL for Pi and scattered beam for Pf)
+    !!----
+    !!----    Calling N the nuclear structure factor (complex scalar) and M* the magnetic
+    !!----    interaction vector (complex vector), the B-M equations are:
     !!----
     !!----    I = N.N* + M.M* + (N.M* + N*.M)Pi - i(M* x M)Pi
     !!----    Pf.I = N.N* Pi - M.M* Pi + (Pi.M*) M + (Pi.M) M* -i(N*.M - N.M*)xPi +
@@ -653,8 +663,8 @@
     !!
     Subroutine Calc_Polar(frame,wave,Cell,UB, Pin, NSF, Mag_dom, Mh, Pf,ok,mess,B_Q)
        !---- Arguments ----!
+       character(len=3),             intent(in)    :: frame
        real(kind=cp),                intent(in)    :: wave
-       character(len=2),             intent(in)    :: frame
        type (Crystal_Cell_Type),     intent(in)    :: Cell
        Real(kind=cp), dimension(3,3),intent(in)    :: UB
        Real(kind=cp), dimension(3),  intent(in)    :: Pin
@@ -670,44 +680,62 @@
        real(kind=cp)                  :: s                 ! sign for T and Wi terms
        real(kind=cp)                  :: I_inv,Inuc,Imag   ! Inverse of elastic cross section
        real(kind=cp)                  :: gamma,omega,nu    ! Normal beam angles
+       real(kind=cp)                  :: pol,ptheta,pchi   ! Module of the incident pol. and angles of nutator + rotation
        complex, dimension (3)         :: MiV, W            ! MiV for one domain and in polarisation frame,Nuclear-Magnetic interference
        integer                        :: nd,ich,nch, ierr
        Real(kind=cp), dimension(3)    :: z1,z4, h, Pic,T,Wr,Wi
-       Real(kind=cp), dimension(3,3)  :: Um,Rot,Rot_omega,ubinv
+       Real(kind=cp), dimension(3,3)  :: Um,Rot,Rot_omega,ubinv,r_pth,r_chi,BL2BM
 
        ok=.true.
        s=1.0_cp
        if(present(B_Q)) s=-1.0_cp
-       z1=Matmul(UB,Mh%h)
+       z1=Matmul(UB,Mh%h) !Cartesian coordinates of reflection hkl when all motors are at zero
        ubinv=invert_A(UB)
-       call Get_Angs_NB(wave,z1,gamma,omega,nu,ierr)
+       call Get_Angs_NB(wave,z1,gamma,omega,nu,ierr)  !Getting normal beam angles from wavelength and z1
        if(ierr /= 0) then
          if(present(mess)) mess="Error calculating the normal beam angles"
          ok=.false.
          return
        else
          if(abs(nu) > 1.0) then !The UB matrix does not correspond to horizontal
-           ok=.false.           !plane scattering geometry
+           ok=.false.           !plane scattering geometry => gamma=2theta
            if(present(mess)) mess="The nu-angle is incompatible with horizontal-plane scattering geometry"
+           return
          end if
        end if
        call Phi_mat(omega,Rot_omega)  !Rotation of the omega motor to put reflection in diffraction position
+                                      !This is counter-clockwise (c s 0|-s c 0|0 0 1)
        z4=matmul(Rot_omega,z1)        !Crystallographic scattering vector in Lab-system when
                                       !the reflection is in diffraction position.
        Rot=Matmul(UB,Matmul(Cell%GD,Cell%Orth_Cr_cel)) !Conversion to BL (in diffraction position) from Crystal Cartesian
 
        Rot=Matmul(Rot_omega,Rot) !Rotation matrix putting the MiVC in the BL frame
 
-       if(frame == "BM" )  then  !Crystallographic Blume-Maleyev frame
-         z4 = z4 / sqrt(dot_product(z4,z4))
+       if(frame(1:2) == "BM" .or. frame(1:3) == "MAD")  then  !Crystallographic Blume-Maleyev frame
+         z4 = z4 / sqrt(dot_product(z4,z4)) !Unit vector along Q in BL system
          Um(1,:) = 0.0
          Um(2,:) = Cross_Product((/0.0,0.0,1.0/),z4)
          Um(3,:) = (/0.0,0.0,1.0/)
          Rot=Matmul(Um,Rot)
-       else if(frame /= "BL" ) then
+       else if(frame(1:2) /= "BL" .and. frame(1:3) /= "MAD") then
          ok=.false.
          if(present(mess)) mess="Undefined reference frame"
          return
+       end if
+       !Put now the incident polarisation in Cartesian coordinates if angular
+       !data have been provided.
+       if(frame == "BLS" .or. frame == "BMS" .or. frame == "MAD") then
+         pol=Pin(1); ptheta=Pin(2); pchi=Pin(3)
+         call chi_mat(ptheta,R_pth) !This is clockwise (c 0 s|0 1 0|-s 0 c)
+         call psi_mat(pchi,R_chi)   !This is counter-clockwise (1 0 0|0 c -s|0 s c)
+         R_Chi=transpose(R_Chi)     !Put chi clockwise
+         Pic=matmul(R_chi,matmul(R_pth,(/0.0,0.0,pol/))) !BL-system incident polarisation
+         if(frame == "BMS" .or. frame == "MAD") then     !Transform to BM system
+           call Phi_mat(gamma*0.5+90.0,BL2BM) !Active
+           Pic=matmul(transpose(BL2BM),Pic)      !Incident polarisation in BM system
+         end if
+       else
+         Pic=Pin
        end if
        nch=1
        if(Mag_Dom%chir) nch=2
@@ -716,24 +744,48 @@
        Inuc=real(Conjg(NSF)*NSF)
        do nd=1,Mag_Dom%nd
          do ich=1,nch
-          MiV=Matmul(Rot,Mh%MiVC(:,ich,nd))  !Convert the MiVC to the frame BM or BL
-          Imag=dot_Product(MiV,MiV)          ! dot_product => MiV*.MiV
-          T=-s*aimag(Cross_Product(Conjg(MiV),MiV)) !Chiral Vector
-          W=2.0*NSF*Conjg(MiV)     !Nuclear-Magnetic Interaction vector
-          Wr=real(W); Wi=s*aimag(W)  !Real and Imaginary parts
-          I_inv=1.0/(Inuc+Imag+dot_product(Wr,Pin)-dot_product(T,Pin))
-          Pf=Pf+ I_inv * Mag_Dom%Pop(ich,nd)*( (Inuc-Imag)*Pin +                  &
-             dot_product(Pin,MiV)*Conjg(MiV) + dot_product(Pin,Conjg(MiV))* MiV + &
-             T + Wr - Cross_Product(Wi,Pin) )
+           MiV=Matmul(Rot,Mh%MiVC(:,ich,nd))  !Convert the MiVC to the frame BM or BL
+           Imag=dot_Product(MiV,MiV)          ! dot_product => MiV*.MiV
+           T=-s*aimag(Cross_Product(Conjg(MiV),MiV)) !Chiral Vector
+           W=2.0*NSF*Conjg(MiV)       !Nuclear-Magnetic Interaction vector
+           Wr=real(W); Wi=s*aimag(W)  !Real and Imaginary parts
+           I_inv=1.0/(Inuc+Imag+dot_product(Wr,Pic)-dot_product(T,Pic))
+           Pf=Pf+ I_inv * Mag_Dom%Pop(ich,nd)*( (Inuc-Imag)*Pic +                  &
+              dot_product(Pic,MiV)*Conjg(MiV) + dot_product(Pic,Conjg(MiV))* MiV + &
+              T + Wr - Cross_Product(Wi,Pic) )
          end do !loop over chiral domains
        end do !loop over S-domains
+
+       !At the end of the calculation Pf is in Cartesian components with respect to
+       !BM or BL system depending if the user wants to get the angles of the scattered
+       !beam nutator and rotation we have to pass first to the Cartesian coordinates
+       !in the scattered beam system. Otherwise the calculated Pf is returned as is.
+       if(frame == "BLS") then   !put polarisation in scattered beam system
+         call Phi_mat(gamma,Rot)
+         Pf = matmul(transpose(Rot) ,Pf)
+       else if(frame == "BMS" .or. frame == "MAD" ) then
+         call Phi_mat(gamma,Rot)
+         Pf = matmul(transpose(Rot),matmul(BL2BM, Pf) )
+       else
+         return
+       end if
+       !Calculate now the angles pchi and ptheta to bring back the polarisation
+       !to the z-direction
+       pol=sqrt(dot_product(Pf,Pf)) !magnitude of scattered polarisation
+       if(abs(pf(3)) < 1.0e-5) then
+         pchi=90.0
+       else
+         pchi=atan2d(pf(2),pf(3))
+       end if
+       ptheta=atan2d(pf(1),pf(2)*sind(pchi)+pf(3)*cosd(pchi))
+       pf=(/pol,ptheta,pchi/)
 
        return
     End Subroutine Calc_Polar
 
-    !!---- Subroutine Get_Pol_Tensor_Pc(wave,Cell,UB,Pin, NSF, Mag_dom, Mh, Pol_tens, Pc,B_Q)
+    !!---- Subroutine Get_Pol_Tensor_Pc(frame,wave,Cell,UB,Pin, NSF, Mag_dom, Mh, Pol_tens, Pc,B_Q)
+    !!----    character(len=3),             intent(in)    :: frame   ! In -> Frame used for polarisation
     !!----    real(kind=cp),                intent(in)    :: wave
-    !!----    character(len=2),             intent(in)    :: frame
     !!----    type (Crystal_Cell_Type),     intent(in)    :: Cell
     !!----    Real(kind=cp), dimension(3,3),intent(in)    :: UB
     !!----    Real(kind=cp), dimension(3),  intent(in)    :: Pin
@@ -745,7 +797,10 @@
     !!----    character(len=*), optional,   intent(in)    :: B_Q  !Original Blume equations are used Q=Q_BM
     !!----
     !!----    This subroutine provides the tensor [P] and vector Pc
-    !!----    in the crystallographic Blume-Maleyev frame
+    !!----    in the crystallographic Blume-Maleyev frame. The input polarisation
+    !!----    may be provided either in BM Cartesian coordinates or using the module
+    !!----    and two angles of rotation (Theta,Chi)corresponding to nutator
+    !!----    and precesion coils in MAD
     !!----    The tensorial form (for whatever reference frame) can be written as:
     !!----    Pf = [P] Pi + Pc => Pc= (Wr + T)/I
     !!----      [P] =  ([D] + [S] + [A])/I
@@ -753,7 +808,8 @@
     !!----      [S] =  [M o M* + M* o M]  -> o indicates tensorial product
     !!----      [A] =  [+i(N.M* - N*.M)]cross = [-Wi]cross
     !!----
-    Subroutine Get_Pol_Tensor_Pc(wave,Cell,UB,Pin, NSF, Mag_dom, Mh, Pol_tens, Pc,ok,mess,B_Q)
+    Subroutine Get_Pol_Tensor_Pc(frame,wave,Cell,UB,Pin, NSF, Mag_dom, Mh, Pol_tens, Pc,ok,mess,B_Q)
+       character(len=3),             intent(in)    :: frame
        real(kind=cp),                intent(in)    :: wave
        type (Crystal_Cell_Type),     intent(in)    :: Cell
        Real(kind=cp), dimension(3,3),intent(in)    :: UB
@@ -770,10 +826,11 @@
        real(kind=cp)                  :: s                 ! sign for T and Wi terms
        real(kind=cp)                  :: I_inv,Inuc,Imag   ! inverse of elastic cross section
        real(kind=cp)                  :: gamma,omega,nu    ! Normal beam angles
+       real(kind=cp)                  :: pol,ptheta,pchi   ! Incident polarisation
        complex, dimension (3)         :: MiV, W     !MiV for one domain and in polarisation frame
        integer                        :: nd,ich,nch, ierr
        Real(kind=cp), dimension(3)    :: z1,z4, h, Pic,T,Wr,Wi
-       Real(kind=cp), dimension(3,3)  :: Um,Rot,Rot_omega,DD,AA,SS
+       Real(kind=cp), dimension(3,3)  :: Um,Rot,Rot_omega,DD,AA,SS,r_pth,r_chi,BL2BM
        Real(kind=cp), dimension(3,3), parameter  :: Identity= reshape ( (/1.0,0.0,0.0, &
                                                                           0.0,1.0,0.0, &
                                                                           0.0,0.0,1.0/),(/3,3/) )
@@ -802,6 +859,19 @@
        Um(2,:) = Cross_Product((/0.0,0.0,1.0/),z4)
        Um(3,:) = (/0.0,0.0,1.0/)
        Rot=Matmul(Um,Rot)
+       !Put now the incident polarisation in Cartesian coordinates w.r.t. BM frame
+       !if angular data have been provided.
+       if(frame == "BLS" .or. frame == "BMS" .or. frame == "MAD") then
+         pol=Pin(1); ptheta=Pin(2); pchi=Pin(3)
+         call chi_mat(ptheta,R_pth) !This is clockwise (c 0 s|0 1 0|-s 0 c)
+         call psi_mat(pchi,R_chi)   !This is counter-clockwise (1 0 0|0 c -s|0 s c)
+         R_Chi=transpose(R_Chi)     !Put chi clockwise
+         Pic=matmul(R_chi,matmul(R_pth,(/0.0,0.0,pol/))) !BL-system incident polarisation
+         call Phi_mat(gamma*0.5+90.0,BL2BM) !Active
+         Pic=matmul(transpose(BL2BM),Pic)   !Incident polarisation in BM system
+       else
+         Pic=Pin
+       end if
        nch=1
        if(Mag_Dom%chir) nch=2
        ! Loop over domains
@@ -815,7 +885,7 @@
           T=-s*aimag(Cross_Product(Conjg(MiV),MiV)) !Chiral Vector
           W=2.0*NSF*Conjg(MiV)     !Nuclear-Magnetic Interaction vector
           Wr=real(W); Wi=s*aimag(W)  !Real and Imaginary parts
-          I_inv=1.0/(Inuc+Imag+dot_product(Wr,Pin)-dot_product(T,Pin))
+          I_inv=1.0/(Inuc+Imag+dot_product(Wr,Pic)-dot_product(T,Pic))
           DD=(Inuc-Imag)*Identity
           SS=real(Tensor_Product(MiV,Conjg(MiV))+Tensor_Product(Conjg(MiV),MiV))
           AA=-Mat_Cross(Wi)
