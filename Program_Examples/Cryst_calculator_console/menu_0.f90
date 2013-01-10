8961f23998dac1fdc617662f8d8a115c0fb41ced
Juan Rodriguez-Carvajal <jrc@ill.fr>
2013-01-10 18:09:02 +0000

    Forgot to commit menu_0.f90!
    git-svn-id: https://forge.ill.fr/svn/crysfml@707 06508225-4b59-0410-a2a9-fdab55894d57

---
+++ Program_Examples/Cryst_calculator_console/menu_0.f90
@@ -0,0 +1,8 @@
+!!----
+!!---- Menu: 0
+!!---- Global items
+ Module Menu_0
+   !---- Use File ----!
+   use CFML_GlobalDeps,  only: OPS
+   character(len=5), public     :: clear_string="clear" !Must be changed to "cls" for Windows
+ End Module Menu_0
