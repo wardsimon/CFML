7ab0621bdd5abf65d2761ef9c46bd50c3d593695
Javier Gonzalez-Platas <jplatas@ull.edu.es>
2015-07-28 21:00:10 +0000

    Updating files...
    git-svn-id: https://forge.ill.fr/svn/crysfml@1169 06508225-4b59-0410-a2a9-fdab55894d57

--- Scripts/makecrysN.bat
+++ Scripts/makecrysN.bat
@@ -9,73 +9,109 @@ rem ---- Default Message ----
 :INIT
    cls
    echo MAKECRYS: Making the CrysFML Library
-   echo Syntax: makecrys [f95/lf95/g95/ifort/gfortran] [con,win,rw] [deb]
-   echo Debug mode cannot be invoked with the "all" option
-   echo The "deb" option has to be provided as second argument for debugging console programs
+   echo Syntax: makecrys [f95/lf95/g95/ifort/ifort15/gfortran] [con,win,rw] [debug]
    goto FINAL
-rem ----
-rem
+rem ---- Variables ----
 :CONT
+   (set _COMPILER=)
+   (set _DEBUG=N)
+   (set _WINTER=N)
+   (set _CONSOLE=N)
+   (set _REALWIN=N)
+rem
+rem ---- Arguments ----
+:LOOP
+    if [%1]==[debug] (set _DEBUG=Y)
+    if [%1]==[win] (set _WINTER=Y)
+    if [%1]==[con] (set _CONSOLE=Y)
+    if [%1]==[rw] (set _REALWIN=Y)
+    if [%1]==[f95] (set _COMPILER=f95)
+    if [%1]==[lf95] (set _COMPILER=lf95)
+    if [%1]==[g95] (set _COMPILER=g95)
+    if [%1]==[ifort] (set _COMPILER=ifort)
+    if [%1]==[gfortran] (set _COMPILER=gfortran)
+    if [%1]==[ifort15] (set _COMPILER=ifort15)
+    shift
+    if not [%1]==[] goto LOOP
+rem
 rem ---- Windows Subdirectory
    cd .\windows
-rem ---- Options ----
-   (set _debug=)
-   if [%3]==[deb] (set _debug=y)
 rem
-   (set _opt1=)
-   if [%2]==[deb] (
-      set _debug=y
-   ) else (
-      if not [%2]==[] (set _opt1=%2)
-   )
-rem
+rem -------------------
 rem ---- Compilers ----
+rem -------------------
 rem
+rem ----
 rem ---- Absoft
-   if [%1]==[f95] (
-      echo Compilador Lahey
-      goto END
-   )
-rem ---- Lahey
-   if [%1]==[lf95] (
-      echo Compilador Lahey
-      goto END
-   )
-rem ---- G95
-   if [%1]==[g95] (
-      echo Compilador Lahey
-      goto END
-   )
-rem ---- Intel
-   if [%1]==[ifort] (
-      if [%_opt1%]==[con] (
-         echo Intel + Console
-         if not [%_debug%]==[Y] (call comp_ifort_co) else (call comp_ifort_cd)
+rem ----
+   if [%_COMPILER%]==[f95] (
+      if [%_CONSOLE%]==[Y] (
+         if [%_DEBUG%]==[Y] (call compile_ifort debug) else (call compile_ifort)
       )
-      if [%_opt1%]==[win] (
-         echo Intel + Wintercater
-         if not [%_debug%]==[Y] (call comp_ifort_wo) else (call comp_ifort_wd)
+      if [%_WINTER%]==[Y] (
+         if [%_DEBUG%]==[Y] (call compile_ifort winter debug) else (call compile_ifort winter)
       )
-      if [%_opt1%]==[rw] (
+      if [%_REALWIN%]==[Y] (
          echo Sorry, option not compatible!!!
          goto END
       )
-      if [%_opt1%]==[] (
-         echo Intel + Console + Wintercater
-         if not [%_debug%]==[Y] (
-            call comp_ifort_co
-            call comp_ifort_wo
-         ) else (
-            call comp_ifort_cd
-            call comp_ifort_wd
-         )
-      )
+      goto END
+   )
+rem ----
+rem ---- Lahey
+rem ----
+   if [%_COMPILER%]==[lf95] (
+      echo Compilador Lahey
+      goto END
+   )
+rem ----
+rem ---- G95
+rem ----
+   if [%_COMPILER%]==[g95] (
+      echo Compilador Lahey
       goto END
    )
 rem ---- GFortran
-   if [%1]==[gfortran] (
-      echo Compilador GFortran
+   if [%_COMPILER%]==[gfortran] (
+      if [%_CONSOLE%]==[Y] (
+         if [%_DEBUG%]==[Y] (call compile_gfortran debug) else (call compile_gfortran)
+      )
+      if [%_WINTER%]==[Y] (
+         if [%_DEBUG%]==[Y] (call compile_gfortran winter debug) else (call compile_gfortran winter)
+      )
+      if [%_REALWIN%]==[Y] (
+         echo Sorry, option not compatible!!!
+         goto END
+      )
       goto END
+   )
+rem ----
+rem ---- Intel
+rem ----
+   if [%_COMPILER%]==[ifort] (
+      if [%_CONSOLE%]==[Y] (
+         if [%_DEBUG%]==[Y] (call compile_ifort debug) else (call compile_ifort)
+      )
+      if [%_WINTER%]==[Y] (
+         if [%_DEBUG%]==[Y] (call compile_ifort winter debug) else (call compile_ifort winter)
+      )
+      if [%_REALWIN%]==[Y] (
+         echo Sorry, option not compatible!!!
+         goto END
+      )
+   )
+
+   if [%_COMPILER%]==[ifort15] (
+      if [%_CONSOLE%]==[Y] (
+         if [%_DEBUG%]==[Y] (call compile_ifort debug 15) else (call compile_ifort 15)
+      )
+      if [%_WINTER%]==[Y] (
+         if [%_DEBUG%]==[Y] (call compile_ifort winter debug 15) else (call compile_ifort winter 15)
+      )
+      if [%_REALWIN%]==[Y] (
+         echo Sorry, option not compatible!!!
+         goto END
+      )
    )
 rem
 :END
