d477c482a374f34bac1c21298e74b19b1c5e5e09
Juan Rodriguez-Carvajal <jrc@ill.fr>
2015-11-16 07:45:54 +0000

    Changes in some scripts to adapt them to the new intel compiler.
    git-svn-id: https://forge.ill.fr/svn/crysfml@1222 06508225-4b59-0410-a2a9-fdab55894d57

--- Scripts/Windows/comp_ifort_co.bat
+++ Scripts/Windows/comp_ifort_co.bat
@@ -7,78 +7,79 @@ rem
    echo **---- Level 0 ----**
    echo .... Mathematical(I), String_Utilities, Messages, Powder Profiles
 rem
-   ifort /c CFML_GlobalDeps_Windows_intel.f90         /O2 /nologo /Qvec-report0
+   ifort /c CFML_GlobalDeps_Windows_intel.f90         /O2 /nologo /Qopt-report:0
 rem
-   ifort /c CFML_math_gen.f90         /O2 /nologo /Qvec-report0
-   ifort /c CFML_LSQ_TypeDef.f90      /O2 /nologo /Qvec-report0
-   ifort /c CFML_spher_harm.f90       /O2 /nologo /Qvec-report0
-   ifort /c CFML_random.f90           /O2 /nologo /Qvec-report0
-   ifort /c CFML_ffts.f90             /O2 /nologo /Qvec-report0
-   ifort /c CFML_string_util.f90      /O2 /nologo /Qvec-report0
-   ifort /c CFML_io_mess.f90          /O2 /nologo /Qvec-report0
-   ifort /c CFML_Profile_TOF.f90      /O2 /nologo /Qvec-report0
-   ifort /c CFML_Profile_Finger.f90   /O2 /nologo /Qvec-report0
-   ifort /c CFML_Profile_Functs.f90   /O2 /nologo /Qvec-report0
+   ifort /c CFML_math_gen.f90         /O2 /nologo /Qopt-report:0
+   ifort /c CFML_LSQ_TypeDef.f90      /O2 /nologo /Qopt-report:0
+   ifort /c CFML_spher_harm.f90       /O2 /nologo /Qopt-report:0
+   ifort /c CFML_random.f90           /O2 /nologo /Qopt-report:0
+   ifort /c CFML_ffts.f90             /O2 /nologo /Qopt-report:0
+   ifort /c CFML_string_util.f90      /O2 /nologo /Qopt-report:0
+   ifort /c CFML_io_mess.f90          /O2 /nologo /Qopt-report:0
+   ifort /c CFML_Profile_TOF.f90      /O2 /nologo /Qopt-report:0
+   ifort /c CFML_Profile_Finger.f90   /O2 /nologo /Qopt-report:0
+   ifort /c CFML_Profile_Functs.f90   /O2 /nologo /Qopt-report:0
 rem
    echo **---- Level 1 ----**
    echo .... Mathematical(II), Optimization, Tables, Patterns
 rem
-   ifort /c CFML_math_3D.f90          /O2 /nologo /Qvec-report0
-   ifort /c CFML_optimization.f90     /O2 /nologo /Qvec-report0
-   ifort /c CFML_optimization_lsq.f90 /O2 /nologo /Qvec-report0
-   ifort /c CFML_sym_table.f90        /Od /nologo /Qvec-report0
-   ifort /c CFML_chem_scatt.f90       /Od /nologo /Qvec-report0
-   ifort /c CFML_BVSpar.f90           /Od /nologo /Qvec-report0
-   ifort /c CFML_diffpatt.f90         /O2 /nologo /Qvec-report0
+   ifort /c CFML_math_3D.f90          /O2 /nologo /Qopt-report:0
+   ifort /c CFML_optimization.f90     /O2 /nologo /Qopt-report:0
+   ifort /c CFML_optimization_lsq.f90 /O2 /nologo /Qopt-report:0
+   ifort /c CFML_sym_table.f90        /Od /nologo /Qopt-report:0
+   ifort /c CFML_chem_scatt.f90       /Od /nologo /Qopt-report:0
+   ifort /c CFML_BVSpar.f90           /Od /nologo /Qopt-report:0
+   ifort /c CFML_diffpatt.f90         /O2 /nologo /Qopt-report:0
 rem
    echo **---- Level 2 ----**
    echo .... Bonds, Crystal Metrics, Symmetry, ILL_Instr
 rem
-   ifort /c CFML_bonds_table.f90      /Od /nologo /Qvec-report0
-   ifort /c CFML_cryst_types.f90      /O2 /nologo /Qvec-report0
-   ifort /c CFML_symmetry.f90         /O2 /nologo /Qvec-report0
-   ifort /c CFML_ILL_Instrm_data.f90  /O2 /nologo /Qvec-report0
+   ifort /c CFML_bonds_table.f90      /Od /nologo /Qopt-report:0
+   ifort /c CFML_cryst_types.f90      /O2 /nologo /Qopt-report:0
+   ifort /c CFML_symmetry.f90         /O2 /nologo /Qopt-report:0
+   ifort /c CFML_ILL_Instrm_data.f90  /O2 /nologo /Qopt-report:0
 rem
    echo **---- Level 3 ----**
-   echo .... Reflections, Atoms
+   echo .... Eos, Reflections, Atoms
 rem
-   ifort /c CFML_Eos_Mod.f90          /O2 /nologo /Qvec-report0
-   ifort /c CFML_reflct_util.f90      /O2 /nologo /Qvec-report0
-   ifort /c CFML_atom_mod.f90         /O2 /nologo /Qvec-report0
+   ifort /c CFML_Eos_Mod.f90          /O2 /nologo /Qopt-report:0
+   ifort /c CFML_reflct_util.f90      /O2 /nologo /Qopt-report:0
+   ifort /c CFML_atom_mod.f90         /O2 /nologo /Qopt-report:0
 rem
    echo **---- Level 4 ----**
-   echo .... Formats
+   echo .... Formats, Geometry Calculations, Molecules
 rem
-   ifort /c CFML_form_cif.f90         /O2 /nologo /Qvec-report0
+   ifort /c CFML_geom_calc.f90        /O2 /nologo /Qopt-report:0
+   ifort /c CFML_molecules.f90        /O2 /nologo /Qopt-report:0
+   ifort /c CFML_form_cif.f90         /O2 /nologo /Qopt-report:0
 rem
    echo **---- Level 5 ----**
-   echo .... Structure Factors, Geometry Calculations, SXTAL geometry, Propag Vectors
+   echo .... Extinction, Structure Factors, SXTAL geometry, Propag Vectors
 rem
-   ifort /c CFML_sfac.f90             /O2 /nologo /Qvec-report0
-   ifort /c CFML_geom_calc.f90        /O2 /nologo /Qvec-report0
-   ifort /c CFML_sxtal_Geom.f90       /O2 /nologo /Qvec-report0
-   ifort /c CFML_propagk.f90          /O2 /nologo /Qvec-report0
+   ifort /c CFML_Extinction_Correction.f90 /O2 /nologo /Qopt-report:0
+   ifort /c CFML_sfac.f90                  /O2 /nologo /Qopt-report:0
+   ifort /c CFML_sxtal_Geom.f90            /O2 /nologo /Qopt-report:0
+   ifort /c CFML_propagk.f90               /O2 /nologo /Qopt-report:0
 rem
    echo **---- Level 6 ----**
-   echo .... Molecules, Maps, BVS, Energy Configurations
+   echo .... Maps, BVS, Energy Configurations
 rem
-   ifort /c CFML_Export_Vtk.f90       /O2 /nologo /Qvec-report0
-   ifort /c CFML_maps.f90             /O2 /nologo /Qvec-report0
-   ifort /c CFML_molecules.f90        /O2 /nologo /Qvec-report0
-   ifort /c CFML_conf_calc.f90        /O2 /nologo /Qvec-report0
+   ifort /c CFML_Export_Vtk.f90       /O2 /nologo /Qopt-report:0
+   ifort /c CFML_maps.f90             /O2 /nologo /Qopt-report:0
+   ifort /c CFML_conf_calc.f90        /O2 /nologo /Qopt-report:0
 rem
    echo **---- Level 7 ----**
    echo .... Magnetic Symmetry, Simulated Annealing, Keywords Parser
 rem
-   ifort /c CFML_magsymm.f90          /O2 /nologo /Qvec-report0
-   ifort /c CFML_optimization_san.f90 /O2 /nologo /Qvec-report0
-   ifort /c CFML_refcodes.f90         /O2 /nologo /Qvec-report0
+   ifort /c CFML_magsymm.f90          /O2 /nologo /Qopt-report:0
+   ifort /c CFML_optimization_san.f90 /O2 /nologo /Qopt-report:0
+   ifort /c CFML_refcodes.f90         /O2 /nologo /Qopt-report:0
 rem
    echo **---- Level 8 ----**
    echo .... Magnetic Structure Factors, Polarimetry
 rem
-   ifort /c CFML_msfac.f90            /O2 /nologo /Qvec-report0
-   ifort /c CFML_polar.f90            /O2 /nologo /Qvec-report0
+   ifort /c CFML_msfac.f90            /O2 /nologo /Qopt-report:0
+   ifort /c CFML_polar.f90            /O2 /nologo /Qopt-report:0
 rem
 rem
    echo **---- Crysfml Library: Console version ----**
--- Scripts/Windows/comp_ifort_wd.bat
+++ Scripts/Windows/comp_ifort_wd.bat
@@ -9,78 +9,78 @@ rem
    echo **---- Level 0 ----**
    echo .... Mathematical,String_Utilities, Messages, Profile Functions
 rem
-   ifort /c CFML_GlobalDeps_Windows_intel.f90         /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_GlobalDeps_Windows_intel.f90         /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
-   ifort /c CFML_math_gen.f90         /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_LSQ_TypeDef.f90      /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_spher_harm.f90       /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_random.f90           /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_ffts.f90             /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_string_util.f90      /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_io_messwin.f90       /debug:full /check /traceback /CB /nologo /Qvec-report0 /I%WINTER%\%LIBFOR%
-   ifort /c CFML_Profile_TOF.f90      /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_Profile_Finger.f90   /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_Profile_Functs.f90   /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_math_gen.f90         /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_LSQ_TypeDef.f90      /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_spher_harm.f90       /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_random.f90           /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_ffts.f90             /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_string_util.f90      /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_io_messwin.f90       /debug:full /check /traceback /CB /nologo /Qopt-report:0 /I%WINTER%\%LIBFOR%
+   ifort /c CFML_Profile_TOF.f90      /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_Profile_Finger.f90   /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_Profile_Functs.f90   /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
    echo **---- Level 1 ----**
    echo .... Mathematical, Optimization, Tables, Patterns
 rem
-   ifort /c CFML_math_3D.f90          /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_optimization.f90     /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_optimization_lsq.f90 /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_sym_table.f90        /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_chem_scatt.f90       /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_BVSpar.f90           /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_diffpatt.f90         /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_math_3D.f90          /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_optimization.f90     /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_optimization_lsq.f90 /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_sym_table.f90        /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_chem_scatt.f90       /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_BVSpar.f90           /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_diffpatt.f90         /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
    echo **---- Level 2 ----**
    echo .... Bonds, Crystal Metrics, Symmetry, ILL_Instr
 rem
-   ifort /c CFML_bonds_table.f90      /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_cryst_types.f90      /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_symmetry.f90         /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_ILL_Instrm_data.f90  /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_bonds_table.f90      /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_cryst_types.f90      /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_symmetry.f90         /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_ILL_Instrm_data.f90  /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
    echo **---- Level 3 ----**
    echo .... Reflections, Atoms
 rem
-   ifort /c CFML_Eos_Mod.f90          /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_reflct_util.f90      /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_atom_mod.f90         /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_Eos_Mod.f90          /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_reflct_util.f90      /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_atom_mod.f90         /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
    echo **---- Level 4 ----**
    echo .... Structure Factors, Geometry Calculations, SXTAL geometry, Propag Vectors
 rem
-   ifort /c CFML_sfac.f90             /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_geom_calc.f90        /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_SXTAL_Geom.f90       /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_propagk.f90          /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_sfac.f90             /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_geom_calc.f90        /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_SXTAL_Geom.f90       /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_propagk.f90          /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
    echo **---- Level 5 ----**
    echo .... Molecules, Maps, BVS, Energy Configurations
 rem
-   ifort /c CFML_molecules.f90        /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_Export_Vtk.f90       /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_maps.f90             /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_conf_calc.f90        /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_molecules.f90        /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_Export_Vtk.f90       /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_maps.f90             /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_conf_calc.f90        /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
    echo **---- Level 6 ----**
    echo .... Formats
 rem
-   ifort /c CFML_form_cif.f90         /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_form_cif.f90         /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
    echo **---- Level 7 ----**
    echo .... Magnetic Symmetry, Simulated Annealing, Keywords Parser
 rem
-   ifort /c CFML_magsymm.f90          /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_optimization_san.f90 /debug:full /check /traceback /CB /nologo /Qvec-report0 /I%WINTER%\%LIBFOR%
-   ifort /c CFML_refcodes.f90         /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_magsymm.f90          /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_optimization_san.f90 /debug:full /check /traceback /CB /nologo /Qopt-report:0 /I%WINTER%\%LIBFOR%
+   ifort /c CFML_refcodes.f90         /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
    echo **---- Level 8 ----**
    echo .... Magnetic Structure Factors, Polarimetry
 rem
-   ifort /c CFML_msfac.f90            /debug:full /check /traceback /CB /nologo /Qvec-report0
-   ifort /c CFML_polar.f90            /debug:full /check /traceback /CB /nologo /Qvec-report0
+   ifort /c CFML_msfac.f90            /debug:full /check /traceback /CB /nologo /Qopt-report:0
+   ifort /c CFML_polar.f90            /debug:full /check /traceback /CB /nologo /Qopt-report:0
 rem
 rem
    echo **---- Crysfml Library: Winteracter version ----**
