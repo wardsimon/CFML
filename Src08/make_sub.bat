f6cb38fd206adeb9ad1ac7af31cd4d36290aaf2d
Javier Gonzalez-Platas <jplatas@ull.edu.es>
2017-01-23 12:06:25 +0000

    Working in progress...
    git-svn-id: https://forge.ill.fr/svn/crysfml@1403 06508225-4b59-0410-a2a9-fdab55894d57

--- Src08/make_sub.bat
+++
@@ -1,9 +0,0 @@
-
-
-ifort /c ext_submod.f90  /debug:full /check /check:noarg_temp_created /traceback /nologo /I. /I"%CRYSFML%"\ifort_debug\LibC
-ifort /exe:ext_submod  *.obj C:\CrysFML\ifort_debug\LibC\crysfml.lib
-
-
-rem ifort /c ext_submod.f90 /O2 /nologo /IC:\CrysFML\ifort\LibC
-rem ifort /exe:ext_submod  *.obj C:\CrysFML\ifort\LibC\crysfml.lib
-del *.obj *.bak
