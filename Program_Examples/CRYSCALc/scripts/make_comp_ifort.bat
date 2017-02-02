6e930f54e6cae018037d337d4828f694a7d583b7
roisnel <roisnel@no_address_provided>
2017-02-02 14:07:02 +0000

    git-svn-id: https://forge.ill.fr/svn/crysfml@1458 06508225-4b59-0410-a2a9-fdab55894d57

--- Program_Examples/CRYSCALc/scripts/make_comp_ifort.bat
+++ Program_Examples/CRYSCALc/scripts/make_comp_ifort.bat
@@ -1,67 +1,83 @@
 
+
 cd ..\src
 
-      ifort      cryscalc_mod.f90        /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      IO_console.f90          /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      text_mod.f90            /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      math_mod.f90            /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      matrix_mod.f90          /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      macros.f90              /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+del *.obj
+del *.mod
+del *.map
 
-      ifort      cryscalc_ext.f90        /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      obv_rev.f90             /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      neutrons_mod.f90        /c /O2    /nologo   /F100000000
-      ifort      xrays_mod.f90           /c /O2    /nologo   /F100000000
-      ifort      atome_mod.f90           /c /O2    /nologo   /F100000000
-      ifort      shannon_mod.f90         /c /O2    /nologo   /F100000000
-      ifort      mag_table.f90           /c /O2    /nologo   /F100000000
-      ifort      calculs.f90             /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      therm.f90               /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      cryscalc_sfac.f90       /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      cryscalc_dist.f90       /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      X_space.f90             /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      mendel.f90              /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      mu_calc.f90             /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      cryscalc_symm.f90       /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      space_group.f90         /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      transf.f90              /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      cryscalc_main_ifort.f90 /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      cryscalc.f90            /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      cryscalc_init.f90       /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      inter_cons.f90          /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      read_CFL.f90            /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      read_INS.f90            /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      read_PCR.f90            /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      read_KEYW.f90           /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      read_CIF_file.f90       /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      create_archive_CIF.f90  /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      SIR_to_INS.f90          /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      read_final_y.f90        /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      read_CELL.f90           /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      niggli.f90              /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      read_SHELX_HKL.f90      /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      create_CFL.f90          /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      search_hkl.f90          /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      search_spgr.f90         /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      sort.f90                /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      stat_data.f90           /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      pgf_file.f90            /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      HELP.f90                /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      create_HTML.f90         /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      create_report.f90       /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
 
-      ifort      create_CIF.f90          /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      cryscalc_lsg_cfml.f90   /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      cryscalc_write.f90      /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      read_nreport.f90        /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
-      ifort      cryscalc_news.f90       /c /O2    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc_mod.f90        /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      IO_console.f90          /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      text_mod.f90            /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      math_mod.f90            /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      matrix_mod.f90          /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      macros.f90              /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc_mod2.f90       /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+rem   ifort      cryscalc_ext.f90        /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc  << renamed as cryscalc_mod2.F90
+      ifort      obv_rev.f90             /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      neutrons_mod.f90        /c /debug:full /check  /traceback    /nologo   /F100000000
+      ifort      xrays_mod.f90           /c /debug:full /check  /traceback    /nologo   /F100000000
+      ifort      atome_mod.f90           /c /debug:full /check  /traceback    /nologo   /F100000000
+rem   ifort      shannon_mod.f90         /c /debug:full /check  /traceback    /nologo   /F100000000         << now included in atom_mod.F90
+rem   ifort      mag_table.f90           /c /debug:full /check  /traceback    /nologo   /F100000000         << now included in atom_mod.F90
+      ifort      calculs.f90             /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      therm.f90               /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc_sfac.f90       /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc_dist.f90       /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      X_space.f90             /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      mendel.f90              /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      mu_calc.f90             /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc_symm.f90       /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      space_group.f90         /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      transf.f90              /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc_main_ifort.f90 /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc.f90            /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+
+REM Get CFML version and date of compilation
+      ifort cryscalc_cfml_ver.f90
+      del  cryscalc_cfml_ver.obj
+      cryscalc_cfml_ver
+      ifort      cc_cfml_ver.f90         /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+REM
+
+
+      ifort      cryscalc_init.f90       /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      inter_cons.f90          /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      read_CFL.f90            /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      read_INS.f90            /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      read_PCR.f90            /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      read_KEYW.f90           /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      read_CIF_file.f90       /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      create_archive_CIF.f90  /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      SIR_to_INS.f90          /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      read_final_y.f90        /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      read_CELL.f90           /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      niggli.f90              /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      read_SHELX_HKL.f90      /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      create_CFL.f90          /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      search_hkl.f90          /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      search_spgr.f90         /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      sort.f90                /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      stat_data.f90           /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      pgf_file.f90            /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      HELP.f90                /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      create_HTML.f90         /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      create_report.f90       /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+
+      ifort      create_CIF.f90          /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc_lsg_cfml.f90   /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc_write.f90      /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      read_nreport.f90        /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
+      ifort      cryscalc_news.f90       /c /debug:full /check  /traceback    /nologo   /F100000000    /I"%CRYSFML%"\ifort\libc
 
 
 
    ifort /exe:cryscalc_ifort *.obj /F100000000 "%CRYSFML%"\ifort\libc\crysfml.lib
-   upx cryscalc_ifort.exe
-   copy cryscalc_ifort.exe ..\..\DistFPS\cryscalc.exe
-   if exist %FULLPROF% copy cryscalc_ifort.exe %FULLPROF%\cryscalc.exe
-   del *.obj *.mod *.bak *.map
+ upx cryscalc_ifort.exe
 
-cd ../scripts/windows
+rem del *.obj
+rem del *.mod
+rem del *.map
+
+rem cd ..\scripts
--- Program_Examples/CRYSCALc/scripts/make_comp_lf95_w.bat
+++ Program_Examples/CRYSCALc/scripts/make_comp_lf95_w.bat
@@ -1,28 +1,31 @@
 
-cd ..\..\src
+cd ..\src
 
 del *.obj
 del *.mod
 del *.map
 
-copy c:\rw_lf95\*.lib
-copy c:\rw_lf95\*.mod
+rem copy c:\rw_lf95\*.lib
+rem copy c:\rw_lf95\*.mod
+
 
 
 
       lf95 -c cryscalc_mod         -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
       lf95 -c IO_rw                -o1 -info  -mod  .;c:\rw_lf95                           -nchk  -nstchk
-      lf95 -c text_mod             -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
+      lf95 -c text_mod             -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c math_mod             -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
       lf95 -c matrix_mod           -o1 -info                                               -nchk  -nstchk
       lf95 -c macros               -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
-      lf95 -c cryscalc_ext         -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
+      lf95 -c cryscalc_mod2        -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
+
+      lf95 -c cryscalc_ext         -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
       lf95 -c obv_rev              -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
       lf95 -c neutrons_mod         -o1 -info                                               -nchk  -nstchk
       lf95 -c xrays_mod            -o1 -info                                               -nchk  -nstchk
       lf95 -c atome_mod            -o1 -info                                               -nchk  -nstchk
-      lf95 -c shannon_mod          -o1 -info                                               -nchk  -nstchk
-      lf95 -c mag_table            -o1 -info                                               -nchk  -nstchk
+rem   lf95 -c shannon_mod          -o1 -info                                               -nchk  -nstchk  <<  included in atome_mod.F90
+rem   lf95 -c mag_table            -o1 -info                                               -nchk  -nstchk  <<  included in atome_mod.F90
       lf95 -c calculs              -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c therm                -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c cryscalc_sfac        -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
@@ -30,11 +33,19 @@ copy c:\rw_lf95\*.mod
       lf95 -c X_space              -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
       lf95 -c mendel               -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
       lf95 -c mu_calc              -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
-      lf95 -c cryscalc_symm        -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
+      lf95 -c cryscalc_symm        -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c space_group          -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
-      lf95 -c transf               -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
+      lf95 -c transf               -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c cryscalc_main_lf95_w -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c cryscalc             -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
+
+REM Get CFML version and date of compilation
+      lf95 cryscalc_cfml_ver
+      del  cryscalc_cfml_ver.obj cryscalc_cfml_ver.map
+      cryscalc_cfml_ver
+      lf95 -c cc_cfml_ver          -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
+REM
+
       lf95 -c cryscalc_init        -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c inter_w              -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c read_CFL             -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
@@ -43,6 +54,7 @@ copy c:\rw_lf95\*.mod
       lf95 -c read_KEYW            -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
       lf95 -c read_CIF_file        -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c create_archive_CIF   -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
+
       lf95 -c SIR_to_INS           -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c read_final_y         -o1 -info  -mod  .;c:\rw_lf95                           -nchk  -nstchk
       lf95 -c read_CELL            -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
@@ -57,7 +69,7 @@ copy c:\rw_lf95\*.mod
       lf95 -c HELP                 -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c create_HTML          -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c create_report        -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
-      lf95 -c create_CIF           -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
+      lf95 -c create_CIF           -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c cryscalc_lsg_cfml    -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
       lf95 -c cryscalc_write       -o1 -info  -mod  .;%CRYSFML%\Lahey\libC;c:\rw_lf95      -nchk  -nstchk
       lf95 -c read_nreport         -o1 -info  -mod  .;%CRYSFML%\Lahey\libC                 -nchk  -nstchk
@@ -65,10 +77,9 @@ copy c:\rw_lf95\*.mod
 
  lf95 -out wcryscalc *.obj   -mod %CRYSFML%\lahey\libC -lib %CRYSFML%\lahey\libC\crysfml;c:\rw_lf95\realwin.lib
  upx wcryscalc.exe
- copy wcryscalc.exe wcryscalc_lf.exe
 
  del *.obj
  del *.mod
  del *.map
 
- cd ..\scripts\Windows
+ cd ..\scripts
