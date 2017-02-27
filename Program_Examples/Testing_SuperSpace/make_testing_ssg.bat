906fabba3fa5e57f2229ef845a2a17b54e145dbc
Juan Rodriguez-Carvajal <jrc@ill.fr>
2017-02-27 10:20:37 +0000

    Adding script for testing SSG.
    git-svn-id: https://forge.ill.fr/svn/crysfml@1483 06508225-4b59-0410-a2a9-fdab55894d57

---
+++ Program_Examples/Testing_SuperSpace/make_testing_ssg.bat
@@ -0,0 +1,28 @@
+@echo off
+rem ****
+rem ****---- Compilation for Testin_SSG Program ----****
+rem ****
+:CONT
+   if x%1 == xgfortran  goto GFOR
+   if x%1 == xifort     goto IFORT
+   goto END
+rem
+rem
+rem ****---- Intel Compiler ----****
+:IFORT
+   ifort /c CFML_ssg_datafile.f90 /debug=full /traceback /nologo /IC:\CrysFML\ifort_debug\LibC
+   ifort /c CFML_SuperSpaceGroups.f90 /debug=full /traceback /nologo /IC:\CrysFML\ifort_debug\LibC
+   ifort /c testing_ssg.f90 /debug=full /traceback /nologo /IC:\CrysFML\ifort_debug\LibC
+   ifort /exe:testing_ssg *.obj C:\CrysFML\ifort_debug\LibC\crysfml.lib
+   goto END
+rem
+rem **---- GFORTRAN Compiler ----**
+:GFOR
+   gfortran -c CFML_ssg_datafile.f90   -I../../GFortran/LibC
+   gfortran -c CFML_SuperSpaceGroups.f90   -I../../GFortran/LibC
+   gfortran -c testing_ssg.f90   -I../../GFortran/LibC
+   gfortran *.o -o testing_ssg    -L../../GFortran/LibC   -lcrysfml
+   goto END
+rem
+:END
+   del *.obj *.mod *.o *.map *.bak > nul
