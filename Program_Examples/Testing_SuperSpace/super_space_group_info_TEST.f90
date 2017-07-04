2dfedc785b19eada58defc95c263690add8da327
rmartinez <rmartinez@no_address_provided>
2017-07-04 08:31:12 +0000

    A cleaner version of the code used to develop Subroutine Set_SSGs_from_Gkk (CFML_SuperSpaceGroups). Her dependencies have been written in CFML_SuperSpaceGroups_TEST.
    git-svn-id: https://forge.ill.fr/svn/crysfml@1549 06508225-4b59-0410-a2a9-fdab55894d57

---
+++ Program_Examples/Testing_SuperSpace/super_space_group_info_TEST.f90
@@ -0,0 +1,174 @@
+!!----
+!!---- Program: Super Space Group Info (SSPG_INFO)
+!!----          Made after the Space Group Info program in  CFML.
+!!----          Restricted to Magnetic SSGs in particular.
+!!---- Author: 
+!!---- Revision: 
+!!
+
+
+
+!>>-------------------------------------------------
+!>> MAIN
+!>>-------------------------------------------------
+
+Program SSPG_Info
+
+  !---- Use Modules ----!
+
+  use CFML_Math_General,              only: Determinant, Equal_Matrix, & 
+                                            Equal_Vector, Modulo_Lat, Zbelong
+  use CFML_Crystallographic_Symmetry, only: Space_Group_Type, set_SpaceGroup, &
+                                            Write_SpaceGroup, Symmetry_Symbol, &
+                                            Get_Generators_From_SpGSymbol,     &
+                                            err_symm, err_symm_mess
+  use CFML_GlobalDeps,                only: cp
+  use CFML_Propagation_Vectors,       only: Group_k_Type, Set_Gk, K_Star, &
+                                            Write_Group_k, k_EQUIV
+  !use CFML_SuperSpaceGroups
+  use CFML_SuperSpaceGroups_MYTEST
+
+
+   !---- Variables ----!
+   implicit none
+
+
+   !---variables for  STEP 1: 
+   !                  Prompts the user to type a SG and a  k-vector
+   !                  Computes the SG's Small Group (or Group of k)
+   !                  The single k has to be written as a general case 
+   !                  of several of them.   
+   !
+   character(len=20)             :: spgr
+   type(Space_Group_type)        :: grp_espacial, SPGk_in, SPGk_out
+   type(Group_k_Type)            :: Gk_out
+   character(len=1)              :: default_example ! len=1 ('y' or 'n')  
+   real(kind=cp), dimension(3)   :: vec_k
+   integer                       :: i,j,k
+   integer                       :: size_Gk
+
+
+   !---variables for  STEP 2:
+   !                  subroutine with all steps to create 
+   !                  all the compatible SSGs with the initial SG+k
+   !
+   integer nkvecs
+   real, dimension(:,:), allocatable                      :: kvecs
+   type(SuperSpaceGroup_Type), dimension(:), allocatable  :: SSGs_out
+   integer                                                :: nSSGs
+
+
+
+   !---- Procedure ----!
+   do
+      WRITE(*,*) " "
+      WRITE(*,*) " "
+      WRITE(*,*) " ========================================================================"
+      WRITE(*,*) " "
+      WRITE(*,*) " STEP 1: find the SMALL GROUP"
+      WRITE(*,*) " "
+
+      ! Overriding with a default input example
+      !
+      !write(unit=*,fmt="(a)") " => Enter a space group: "
+      !write(unit=*,fmt="(a)",advance="no") " => Space Group (HM/Hall symbol or number): "
+      !read(unit=*,fmt="(a)") spgr
+      !overriding: default example
+      !spgr="P n m a"
+      !WRITE(*,*) "your input:", spgr
+
+      !if (len_trim(spgr)==0) exit
+      !write(unit=*,fmt="(a)",advance="no") " => Enter a a k-vector: "
+      !read(unit=*,fmt=*) vec_k
+      !overriding: default example
+      !vec_k=(/ 0.321, 0.0, 0.5 /)
+
+      spgr=""
+      vec_k=0
+      write(unit=*,fmt="(a)") " => Would you like the default Space Group and h-vector"
+      write(unit=*,fmt="(a)") " => P n m a"
+      write(unit=*,fmt="(a)") " => 0.321 0 0.5"
+      write(unit=*,fmt="(a)") " => y/n?"
+      read(unit=*,fmt=*) default_example
+      if(default_example=="y") then
+          write(unit=*,fmt="(a)") "Using default example"
+          spgr="P n m a"
+          vec_k=(/ 0.321, 0.0, 0.5 /)
+      else
+          !> New prompt
+          write(unit=*,fmt="(a)") " => Enter a space group: "
+          write(unit=*,fmt="(a)",advance="no") " => Space Group (HM/Hall symbol or number): "
+          read(unit=*,fmt="(a)") spgr
+          if (len_trim(spgr)==0) exit
+          write(unit=*,fmt="(a)",advance="no") " => Enter a k-vector: "
+          read(unit=*,fmt=*) vec_k
+      end if
+
+      !> Setting and writing the SG Information
+      !> Casting the SG variable to decouple this part of the code from the following
+      WRITE(*,*) " setting and writing grp_espacial:"
+      call Set_SpaceGroup(spgr, grp_espacial)
+      call Write_SpaceGroup(grp_espacial, full=.true.)
+      size_Gk=grp_espacial%multip  
+      SPGk_in=grp_espacial ! casting
+      
+ 
+WRITE(*,*) " "
+WRITE(*,*) " ========================================================================"
+WRITE(*,*) " "
+WRITE(*,*) " STEP 2:  using Set_SSGs_from_Gkk"
+WRITE(*,*) " "
+
+      
+
+!--- the final goal is to write Set_SSGs_from_Gkk
+!    we adapt here the new parameters: nk, kv
+!  
+if(allocated(kvecs)) deallocate(kvecs) 
+allocate(kvecs(1,3))
+
+!--- testing with 1 k-vec
+!
+nkvecs=1 
+kvecs(1,:)=vec_k 
+
+!--- testing with 2 k-vecs
+!
+!nkvecs=2 
+!kvecs(1,:)=vec_k 
+!kvecs(2,:)=vec_k + [0.201, 0., 0.]
+
+
+!---++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!
+call Set_SSGs_from_Gkk_MYTEST(SPGk_in, nkvecs, kvecs, SSGs_out, nSSGs)
+!
+!---++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+! 
+write(*,*) "final results:"
+write(*,*) "num of SSGSs", nSSGs
+do i=1, nSSGs
+ call Write_SSG(SSGs_out(i),full=.true.)
+enddo 
+!
+!---++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  
+
+!> NEXT STEPS
+!>
+!> MSSG types from SSGs
+
+WRITE(*,*) " "
+WRITE(*,*) " ========================================================================"
+WRITE(*,*) " "
+
+
+
+
+end do ! prompting loop
+
+
+
+!!----------------------------------------------------------------------------
+
+End Program SSPG_Info
