16ffaacbdb775ad36ebde9c637352939b3facafc
Juan Rodriguez-Carvajal <jrc@ill.fr>
2018-03-12 21:42:46 +0000

    Deleting files x-ray-mag
    git-svn-id: https://forge.ill.fr/svn/crysfml@1605 06508225-4b59-0410-a2a9-fdab55894d57

--- Program_Examples/Magnetism/X-ray-Mag/XRMS_ref/compile.bat
+++
@@ -1,7 +0,0 @@
-cls
-gfortran -c XRMS_types.f90 -I%crysfml%/gfortran/libc 
-gfortran -c global_data.f90 -I%crysfml%/gfortran/libc 
-gfortran -c XRMS_procedures.f90 -I%crysfml%/gfortran/libc 
-gfortran -c XRMS_ref.f90 -I%crysfml%/gfortran/libc 
-gfortran *.o -o XRMS_ref -L%crysfml%/gfortran/libc -lcrysfml
-del *.o *.mod > nul
--- Program_Examples/Magnetism/X-ray-Mag/XRMS_ref/test.bat
+++
@@ -1,7 +0,0 @@
-cls
-gfortran -c XRMS_types.f90 -I%crysfml%/gfortran/libc 
-gfortran -c global_data.f90 -I%crysfml%/gfortran/libc 
-gfortran -c XRMS_procedures.f90 -cpp -I%crysfml%/gfortran/libc 
-gfortran -c XRMS_ref_test.f90 -cpp -I%crysfml%/gfortran/libc 
-gfortran *.o -o XRMS_ref_test -L%crysfml%/gfortran/libc -lcrysfml
-del *.o *.mod > nul
