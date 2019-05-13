114bf906d22463d437d7f09dccf237a1ff653f3b
Perenon R <perenon@ill.fr>
2019-05-13 15:56:13 +0200

    Debugging CI for Windows

--- BuildServer/Windows/build.bat
+++ BuildServer/Windows/build.bat
@@ -1,13 +1,9 @@
 set compiler=%1
 echo "Building CrysFML with %compiler%"
 
-set OPTIONS=""
-if %compiler% neq ifort (
-    set OPTIONS=-G "NMake Makefiles"
-)
 rmdir build_%compiler% /s /q
 mkdir build_%compiler%
 cd build_%compiler%
 
-cmake %OPTIONS% -D CMAKE_Fortran_COMPILER=%compiler% ..
+cmake -G "NMake Makefiles" -D CMAKE_Fortran_COMPILER=%compiler% ..
 cmake --build .
