8aafb52bdb233cfa23d039f1f5aa89298965282e
Perenon R <perenon@ill.fr>
2019-06-07 15:20:55 +0200

    Setting back HDF

--- BuildServer/Windows/build.bat
+++ BuildServer/Windows/build.bat
@@ -6,9 +6,9 @@ mkdir build_%compiler%
 cd build_%compiler%
 
 if %compiler% neq gfortran (
-cmake -G "NMake Makefiles" -D CMAKE_Fortran_COMPILER=%compiler% ..
+cmake -G "NMake Makefiles" -D USE_HDF=ON -D CMAKE_Fortran_COMPILER=%compiler% ..
 ) else (
-cmake -G "NMake Makefiles" -D CMAKE_Fortran_COMPILER=%compiler% -D CMAKE_CXX_COMPILER=g++.exe -D CMAKE_C_COMPILER=gcc.exe  -D MINGW_HDF_TRICK=1 ..
+cmake -G "NMake Makefiles" -D USE_HDF=ON -D CMAKE_Fortran_COMPILER=%compiler% -D CMAKE_CXX_COMPILER=g++.exe -D CMAKE_C_COMPILER=gcc.exe  -D MINGW_HDF_TRICK=1 ..
 )
 
 cmake --build .
