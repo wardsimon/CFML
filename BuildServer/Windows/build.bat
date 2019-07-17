6bbca797ad92c947e15efbda63a69ca4f3ce12be
Juan Rodriguez-Carvajal <rodriguez-carvajal@ill.eu>
2019-07-17 12:03:10 +0200

    Correcting PSD_Convert subroutine in CFML_SXTAL_Geom.f90

--- BuildServer/Windows/build.bat
+++ BuildServer/Windows/build.bat
@@ -10,7 +10,7 @@ cmake -G "NMake Makefiles" -D USE_HDF=ON -D CMAKE_Fortran_COMPILER=%compiler% ..
 ) else (
 rem C++ and C compiles must be set. Also, a "trick" has been set on the build server to build with MinGW
 rem See See http://hdf-forum.184993.n3.nabble.com/HDF5-and-MinGW-td3393676.html for more details about this "trick"
-cmake -G "NMake Makefiles" -D USE_HDF=ON -D CMAKE_Fortran_COMPILER=%compiler% -D CMAKE_CXX_COMPILER=g++.exe -D CMAKE_C_COMPILER=gcc.exe  -D MINGW_HDF_TRICK=1 ..
+cmake -G "MinGW Makefiles" -D USE_HDF=ON -D CMAKE_Fortran_COMPILER=%compiler% -D CMAKE_CXX_COMPILER=g++.exe -D CMAKE_C_COMPILER=gcc.exe  -D MINGW_HDF_TRICK=1 ..
 )
 
 cmake --build .
