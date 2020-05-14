d016b9832acbcc6fa3d256033919afb3780be177
Perenon R <perenon@ill.fr>
2020-05-14 22:59:39 +0200

    Debug docker, change in Docker clean script to remove volumes, and do not download artifacts for post-CI Docker clean

--- BuildServer/Docker/clean.sh
+++ BuildServer/Docker/clean.sh
@@ -1,5 +1,5 @@
 # Return the docker containers that are exited
-exited_containers=`docker ps -q "status=exited"`
+exited_containers=`docker ps -q -f "status=exited"`
 # If some have been found, remove them
 if [ -n "$exited_containers" ]
 then
@@ -7,9 +7,17 @@ then
 fi
 
 # Return the docker images that are in dangling state
-images=`docker images -q "dangling=true"`
+images=`docker images -q -f "dangling=true"`
 # If some dangling images have been found, remove them
 if [ -n "$images" ]
 then
-    docker rmi -f ${images}
+    docker rmi ${images}
+fi
+
+# Also remove docker volumes
+volumes=`docker volume ls -q -f "dangling=true"`
+# If some dangling volumes have been found, remove them
+if [ -n "$volumes" ]
+then
+    docker volume rm ${volumes}
 fi
