1944102c9a57a1c0a906a2492941c2faad24303d
Perenon R <perenon@ill.fr>
2020-05-14 15:05:01 +0200

    Debugging CI and build scripts to compel with Fortran HDF5

--- BuildServer/Unix/build.sh
+++ BuildServer/Unix/build.sh
@@ -5,5 +5,5 @@ rm -Rf build_${compiler##/*}
 mkdir build_${compiler##/*}
 cd build_${compiler##/*}
 
-cmake -D USE_HDF=ON -D CMAKE_Fortran_COMPILER=${compiler} ..
+cmake -D USE_HDF=ON -D CMAKE_Fortran_COMPILER=${compiler} -D HDF5_INCLUDE_PATH=${HDF5_INCLUDE_PATH} ..
 cmake --build .
