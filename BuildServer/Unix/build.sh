380a4a22f3f1f158d66bc40be612d156f02bb8ae
Perenon R <perenon@ill.fr>
2020-05-19 20:42:47 +0200

    Refactoring CI such as build is two-staged, one stage is Debug and next is Release

--- BuildServer/Unix/build.sh
+++ BuildServer/Unix/build.sh
@@ -5,5 +5,5 @@ rm -Rf build_${compiler##/*}
 mkdir build_${compiler##/*}
 cd build_${compiler##/*}
 
-cmake -D ARCH32=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_Fortran_COMPILER=${compiler} -D USE_HDF=ON -D HDF5_INCLUDE_PATH=${HDF5_INCLUDE_PATH} -D HDF5_LIBRARY_PATH=${HDF5_LIBRARY_PATH} ..
+cmake -D ARCH32=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_Fortran_COMPILER=${compiler} -D USE_HDF=ON -D HDF5_INCLUDE_PATH=${HDF5_INCLUDE_PATH} -D HDF5_LIBRARY_PATH=${HDF5_LIBRARY_PATH} ..
 cmake --build .
