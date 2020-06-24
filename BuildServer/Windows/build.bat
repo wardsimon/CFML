948272d47228ef52700436126967fa20f9e9084b
Perenon R <perenon@ill.fr>
2020-06-24 21:59:23 +0200

    Adding HEAP_ARRAYS option and use it for cmake ifort on Windows

--- BuildServer/Windows/build.bat
+++ BuildServer/Windows/build.bat
@@ -6,7 +6,7 @@ mkdir build_%compiler%
 cd build_%compiler%
 
 if %compiler% neq gfortran (
-cmake -G "NMake Makefiles" -D ARCH32=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_Fortran_COMPILER=%compiler% -D USE_HDF=ON -D HDF5_INCLUDE_PATH=%HDF5_INCLUDE_PATH% -D HDF5_LIBRARY_PATH=%HDF5_LIBRARY_PATH% ..
+cmake -G "NMake Makefiles" -D ARCH32=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_Fortran_COMPILER=%compiler% -D HEAP_ARRAYS=ON -D USE_HDF=ON -D HDF5_INCLUDE_PATH=%HDF5_INCLUDE_PATH% -D HDF5_LIBRARY_PATH=%HDF5_LIBRARY_PATH% ..
 ) else (
 cmake -G "MinGW Makefiles" -D ARCH32=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_Fortran_COMPILER=%compiler% -D USE_HDF=OFF -D HDF5_INCLUDE_PATH=%HDF5_INCLUDE_PATH% -D HDF5_LIBRARY_PATH=%HDF5_LIBRARY_PATH% ..
 )
