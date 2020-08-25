7b4e343e63ce1c3fac8a4b6b1e38f96ef1c59a6d
Elisa Rebolini <rebolini@ill.fr>
2020-08-25 10:25:53 +0200

    type generators for atom and space group

---
+++ Python_API/Derived_types_generator/generate_atom.py
@@ -0,0 +1,74 @@
+from DerivedType import DerivedType
+
+d = DerivedType("Atom", "Atom_Type", "atom_typedef")
+
+
+d.addParam("label","Lab","string","label")         
+d.addParam("chemical_symbol","ChemSymb","string","Chemical Symbol")    
+d.addParam("sfac_symbol","SfacSymb","string","Symbol for Scattering Factor")
+d.addParam("wyckoff_letter","wyck","string","Wyckoff letter")
+
+d.addParam("active","Active","bool","Flag to control is the atom is included or excluded")
+
+d.addParam("atomic_number","Z","object","Atomic number")
+d.addParam("site_multiplicity","Mult","object","Multiplicity of the site")
+
+d.addParam("xyz","X","ndarray","Fractional coordinates")
+d.addParam("xyz_std_dev","X_Std","ndarray","Standard deviation of the fractional coordinates")
+d.addParam("multiplier_xyz","MX","ndarray","Multipliers for xyz")
+d.addParam("lsq_xyz","LX","ndarray","Labels in the LSQ list (refined parameters) for xyz")
+
+d.addParam("occ","Occ","object","Occupation factor")
+d.addParam("occ_std_dev","Occ_Std","object","Standard deviation of the occupation factor")
+d.addParam("multiplier_occ","MOcc","object","Multiplier for occ")
+d.addParam("lsq_occ","LOcc","object","Label in the LSQ list for occ")
+
+d.addParam("biso","Biso","object","Isotropic B-factor")
+d.addParam("biso_std_dev","Biso_std","object","Standard deviation for biso")
+d.addParam("multiplier_biso","MBiso","object","Multiplier for biso")
+d.addParam("lsq_biso","LBiso","object","Label in the LSQ list for biso")
+
+
+
+# Betas are defined by the following expression of the temperature factor:
+# Taniso= exp( -(beta11 h^2 + beta22 k^2 + beta33 l^2 + 2 (beta12 h k + beta13 h l + beta23 k l)) )
+# Taniso= exp( -(bet(1) h^2 + bet(2) k^2 + bet(3) l^2 + 2 (bet(4) h k + bet(5) h l + bet(6) k l)) )
+
+# Us are defined by the following expression of the temperature factor:
+# Taniso= exp( -2pi^2 (h^2 (a*)^2 U11+ k^2 (b*)^2 U22+ l^2 (c*)^2 U33+
+#                2 (h k (a*) (b*) U12+ h l (a*) (c*) U13+  k l (b*) (c*) U23)) )
+
+# Us and Bs are related by a scaling factor B = 8*Pi^2 U
+
+d.addParam("u_type","Utype","string","Type of anisotropic thermal parameters: u_ij, b_ij, beta, none")
+d.addParam("thermal_type","ThType","string","Type of behavior wrt to the temperature: isotr, aniso or other")
+
+d.addParam("u_array","U","ndarray","Array of anisotropic thermal coefficients U11, U22, U33, U12, U13, U23 or B11, B22, B33, B12, B13, B23 depending on the value of Utype")
+d.addParam("u_array_std_dev","U_std","ndarray","Array of standard deviation of the anisotropic thermal coefficients")
+d.addParam("multiplier_u_array","MU","ndarray","Array of multipliers for the anisotropic thermal coefficients")
+d.addParam("lsq_u_qrrqy","LU","ndarray","Array of labels in the LSQ list for the anisotropic thermal coefficients")
+
+d.addParam("u_equiv","Ueq","object","Equivalent isotropic atomic displacement parameter, U(equiv), in angstroms squared, calculated from anisotropic atomic displacement parameters.
+U(equiv) = (1/3) sum~i~[sum~j~(U^ij^ a*~i~ a*~j~ a~i~ a~j~)]")
+
+d.addParam("formal_charge","Charge","object","The net integer charge assigned to this atom. This is the formal charge assignment normally found in chemical diagrams.")
+d.addParam("moment","Moment","object","Theoretical value of the magnetic moment. i.e., number of unpaired electrons for transition metal oxides")
+
+d.addParam("index_sfac","Ind","ndarray","Index for different purposes (e.g. 1:pointer to the scattering factor, 2:pointer to the magnetic scattering factor")
+
+d.addParam("nvarF","NVar","object","Dimension of VarF")
+d.addParam("varF","VarF","ndarray","Free variables used for different purposes (1,2,3 reserved for occupations, not refinable)")
+d.addParam("multiplier_varF","MVarF","ndarray","Multipliers of the free variables varF")
+d.addParam("lsq_varF","LVarF","ndarray","Labels in the LSQ list of the free variables varF")
+
+d.addParam("info","AtmInfo","string","Information String")
+
+
+d.addParam("m_xyz","m_xyz","ndarray","Magnetic moment along x,y,z")
+d.addParam("m_xyz_std_dev","sm_xyz","ndarray","Standard deviation of the magnetic moment along x,y,z")
+d.addParam("multiplier_m_xyz","Mm_xyz","ndarray","Multipliers of the magnetic moment along x,y,")
+d.addParam("lsq_m_xyz","Lm_xyz","ndarray","Label in the LSQ list of the magnetic moment along x,y,")
+
+d.generate_template("file_atom")
+
+
---
+++ Python_API/Derived_types_generator/generate_spg.py
@@ -0,0 +1,27 @@
+from DerivedType import DerivedType
+
+d = DerivedType("SpaceGroup", "Space_Group_Type", "crystallographic_symmetry")
+
+d.addParam("hall_symbol","hall","string","Hall symbol")
+d.addParam("generalized_hall_symbol", "ghall","string","Generalised Hall symbol")
+d.addParam("crystal_system","CrystalSys","string","Crystal System")
+d.addParam("laue_class","Laue","string","Laue class")
+d.addParam("point_group","PG","string","Point group")
+d.addParam("extra_information","Info","string","Extra Information")
+d.addParam("space_group_setting","SG_setting","string","Information about the SG setting (IT,KO,ML,ZA,Table,Standard,UnConventional)")
+d.addParam("space_group_lattice_type","SPG_lat","string","Lattice type")
+d.addParam("space_group_lattice_type_symbol","SPG_latsy","string","Lattice type Symbol")
+d.addParam("number_of_lattice_points","NumLat","object","Number of lattice points in a cell")
+d.addParam("bravais","bravais","string","String with Bravais symbol + translations")
+d.addParam("centre","centre","string","Alphanumeric information about the center of symmetry")
+d.addParam("centric","centred","object","Centric or Acentric [ =0 Centric(-1 no at origin),=1 Acentric,=2 Centric(-1 at origin)]")
+d.addParam("inversion_centre","centre_coord","ndarray","Fractional coordinates of the inversion centre")
+d.addParam("number_of_symops","numops","object","Number of reduced set of Symmetry Operators")
+d.addParam("multiplicity","multip","object","Multiplicity of the general position")
+d.addParam("operators_minimum_number","num_gen","object","Minimum number of operators to generate the Group")
+d.addParam("symmetry_operators","SymOp","object","Symmetry operators (192)")
+d.addParam("symmetry_operators_as_text","SymopSymb","object","String form of symmetry operators")
+d.addParam("wyckoff_info","wyckoff","object","Wyckoff Information")
+d.addParam("asymetric_unit","R_asym_unit","ndarray","Asymmetric unit in real space")
+
+d.generate_template("file_spg")
