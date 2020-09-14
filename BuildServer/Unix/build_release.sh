e113cafc2bfb98af71aadd624894d3044b2d626c
Perenon R <perenon@ill.fr>
2020-09-14 16:38:57 +0200

    Adding documentation within CI

--- BuildServer/Unix/build_release.sh
+++ BuildServer/Unix/build_release.sh
@@ -6,5 +6,7 @@ mkdir build_release
 cd build_release
 
 cmake -D ARCH32=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_Fortran_COMPILER=ifort -DPYTHON_API=ON -DPYTHON_LIBRARY_PATH=${PYTHON_LIBRARY_PATH} -D USE_HDF=ON -D HDF5_INCLUDE_PATH=${HDF5_INCLUDE_PATH} -D HDF5_LIBRARY_PATH=${HDF5_LIBRARY_PATH} ..
-cmake --build . --target install
+cmake --build . 
+cmake --build . --target doxygen
 ctest
+cmake --build . --target install
--- BuildServer/Windows/build_release.bat
+++ BuildServer/Windows/build_release.bat
@@ -6,5 +6,7 @@ mkdir build_release
 cd build_release
 
 cmake -G "NMake Makefiles" -D ARCH32=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_Fortran_COMPILER=ifort -D HEAP_ARRAYS=ON -D PYTHON_API=ON -D PYTHON_LIBRARY_PATH=%PYTHON_LIBRARY_PATH% -D USE_HDF=ON -D HDF5_INCLUDE_PATH=%HDF5_INCLUDE_PATH% -D HDF5_LIBRARY_PATH=%HDF5_LIBRARY_PATH% ..
-cmake --build . --target install
+cmake --build . 
+cmake --build . --target doxygen
 ctest
+cmake --build . --target install
