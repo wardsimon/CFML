deb688549f561fa8da08a9286065385966c460eb
Elisa Rebolini <rebolini@ill.fr>
2020-10-06 16:53:51 +0200

    test with setters and prints

--- Python_API/Examples/test_atom_from_string.py
+++ Python_API/Examples/test_atom_from_string.py
@@ -16,6 +16,10 @@ atom_list = cif_file.atom_list
 
 atom = atom_list[0]
 
+atom_list.print_description()
+
 atom.from_string('ATOM C C 0.0 0.0 0.0 0.2 0.5')
 
-print(atom.label)
+atom_list[0] = atom 
+
+atom_list.print_description()
