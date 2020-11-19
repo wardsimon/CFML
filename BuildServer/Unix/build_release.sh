52a25d954a211bbc4170be85f0e8e53425be74a8
Perenon R <perenon@ill.fr>
2020-11-19 22:25:02 +0100

    Adding gfortran compilation for Linux release

--- BuildServer/Unix/build_release.sh
+++ BuildServer/Unix/build_release.sh
@@ -1,11 +1,15 @@
 compiler=$1
-echo "Building CrysFML with ${compiler*##/}"
+if [ -z "${compiler}" ]; then
+    compiler=ifort
+fi
+
+echo "Building CrysFML with ${compiler##*/}"
 
 rm -Rf build_release
 mkdir build_release
 cd build_release
 
-cmake -D ARCH32=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_Fortran_COMPILER=ifort -DPYTHON_API=ON -DPYTHON_INTERPRETER_PATH=${PYTHON_INTERPRETER_PATH} -DPYTHON_LIBRARY_PATH=${PYTHON_LIBRARY_PATH} -D USE_HDF=ON -D HDF5_INCLUDE_PATH=${HDF5_INCLUDE_PATH} -D HDF5_LIBRARY_PATH=${HDF5_LIBRARY_PATH} ..
+cmake -D ARCH32=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_Fortran_COMPILER=${compiler} -DPYTHON_API=ON -DPYTHON_INTERPRETER_PATH=${PYTHON_INTERPRETER_PATH} -DPYTHON_LIBRARY_PATH=${PYTHON_LIBRARY_PATH} -D USE_HDF=ON -D HDF5_INCLUDE_PATH=${HDF5_INCLUDE_PATH} -D HDF5_LIBRARY_PATH=${HDF5_LIBRARY_PATH} ..
 cmake --build . 
 cmake --build . --target doxygen
 eval "${TEST_TRICK_COMMAND}"
