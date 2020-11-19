Fixing the sin_theta_over_lambda_max calculation

Python_API/Src/PowderPatternSimulation.py
@@ -49,8 +49,8 @@ class PowderPatternSimulationConditions():
         }
         
     def getSinThetaOverLambdaMax(self):
-        angle_max = min((self.theta_max+10.0)*0.5,90.0) * 180 / np.pi
-        return np.sin(angle_max/self.lamb)
+        angle_max = min((self.theta_max+10.0)*0.5,90.0) / 180 * np.pi
+        return np.sin(angle_max)/self.lamb
     
     def readFromCFLFile(self, file_name):
         with(open(file_name, "r")) as f: